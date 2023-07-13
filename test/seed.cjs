6dbe7f0c2bc1b3fc4da42b6a18fc73eacaabe6ad
Victor Mimo <victormimoc@gmail.com>
2023-07-13 15:44:15 -0300

    Adding stock legends and stock plans to seed

--- test/seed.cjs
+++ test/seed.cjs
@@ -1,6 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const inputStakeholders = require('../ocf/samples/Stakeholders.ocf.json') 
 const inputStockClasses = require('../ocf/samples/StockClasses.ocf.json')
+const inputStockLegends = require('../ocf/samples/StockLegends.ocf.json')
+const inputStockPlans = require('../ocf/samples/StockPlans.ocf.json')
 
 // manifest wraps all of these.
 
@@ -9,7 +11,7 @@ const prisma = new PrismaClient();
 async function main() {
 
         // STAKEHOLDER
-        console.log('Adding Stakeholder to DB')
+        console.log('Adding Stakeholders to DB')
         for (const inputStakeholder of inputStakeholders.items) {
             const stakeholder = await prisma.stakeholder.create({
                 data: inputStakeholder
@@ -19,7 +21,7 @@ async function main() {
         }
     
         // STOCK CLASS
-        console.log('Adding Stock Class to DB')
+        console.log('Adding Stock Classes to DB')
         for (const inputStockClass of inputStockClasses.items) {
             const stockClass = await prisma.stockClass.create({
                 data: inputStockClass
@@ -28,20 +30,27 @@ async function main() {
             console.log('Stockclass added ', stockClass)
         }
 
+        // STOCK LEGEND
+        console.log('Adding Stock Legends to DB')
+        for (const inputStockLegend of inputStockLegends.items) {
+            const stockLegend = await prisma.stockLegendTemplate.create({
+                data: inputStockLegend
+            })
 
-    // const issuer = await prisma.issuer.create({
-    //     data: {
-    //         legal_name: "Poet Network Inc.",
-    //         dba: "Poet",
-    //         formation_date: "2022-08-23",
-    //         country_of_formation: "US",
-    //         country_subdivision_of_formation: "DE",
-    //         tax_ids: ["12-315-69"],
-    //         email: "concierge@poet.network",
-    //         phone: "212-111-111",
-    //         initial_shares_authorized: "10000000",
-    //     },
-    // });
+            console.log('Stock legend added ', stockLegend)
+        }
+
+        // STOCK PLAN
+        console.log('Adding Stock Plan to DB')
+        for (const inputStockPlan of inputStockPlans.items) {
+            const stockPlan = await prisma.stockPlan.create({
+                data: inputStockPlan
+            })
+
+            console.log('Stock plan added ', stockPlan)
+        }
+
+
 
 }
 
