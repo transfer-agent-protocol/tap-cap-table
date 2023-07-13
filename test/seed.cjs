5bec13a2e213ace7cd15a18cb3ee569255509781
Victor Mimo <victormimoc@gmail.com>
2023-07-13 16:33:27 -0300

    adding valuations and vesting terms or testing

--- test/seed.cjs
+++ test/seed.cjs
@@ -4,6 +4,7 @@ const inputStockClasses = require('../ocf/samples/StockClasses.ocf.json')
 const inputStockLegends = require('../ocf/samples/StockLegends.ocf.json')
 const inputStockPlans = require('../ocf/samples/StockPlans.ocf.json')
 const inputValuations = require('../ocf/samples/Valuations.ocf.json')
+const inputVestingTerms = require('../ocf/samples/VestingTerms.ocf.json')
 
 // manifest wraps all of these.
 
@@ -41,8 +42,8 @@ async function main() {
             console.log('Stock legend added ', stockLegend)
         }
 
-        // STOCK PLAN
-        console.log('Adding Stock Plan to DB')
+        // STOCK PLANS
+        console.log('Adding Stock Plans to DB')
         for (const inputStockPlan of inputStockPlans.items) {
             const stockPlan = await prisma.stockPlan.create({
                 data: inputStockPlan
@@ -51,14 +52,24 @@ async function main() {
             console.log('Stock plan added ', stockPlan)
         }
 
-        // VALUATION
-        console.log('Adding Valuation to DB')
+        // VALUATIONS
+        console.log('Adding Valuations to DB')
         for (const inputValuation of inputValuations.items) {
-            const valuation = await prisma.valuation.create({
+            const valuation = await prisma.valuations.create({
                 data: inputValuation
             })
 
             console.log('Valuation added ', valuation)
+        }
+
+        // VESTING TERMS
+        console.log('Adding Vesting Terms to DB')
+        for (const inputVestingTerm of inputVestingTerms.items) {
+            const vestingTerm = await prisma.vestingTerms.create({
+                data: inputVestingTerm
+            })
+
+            console.log('Vesting term added ', vestingTerm)
         }
 
 
