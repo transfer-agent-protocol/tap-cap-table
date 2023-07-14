548bbb8cf3256ddc3ab82407dfd64526685107d3
Victor Mimo <victormimoc@gmail.com>
2023-07-14 06:43:15 -0300

    Creating a deseed test script to delete dummy data from local DB for faster testing.

---
+++ test/deseed.cjs
@@ -0,0 +1,41 @@
+const prisma = new PrismaClient()
+
+async function main() {
+
+        //TODO: only execute if running on local DB instance
+    
+        const deleteAllUsers = await prisma.user.deleteMany()
+        console.log(`Deleted ${deleteAllUsers.count} users`)
+
+        // STAKEHOLDER
+        const deleteAllStakeholders = await prisma.stakeholder.deleteMany()
+        console.log(`Deleted ${deleteAllStakeholders.count} stakeholders`)
+
+        // STOCK CLASS
+        const deleteAllStockClasses = await prisma.stockClass.deleteMany()
+        console.log(`Deleted ${deleteAllStockClasses.count} stock classes`)
+
+        // STOCK LEGEND
+        const deleteAllStockLegends = await prisma.stockLegendTemplate.deleteMany()
+        console.log(`Deleted ${deleteAllStockLegends.count} stock legends`)
+
+        // STOCK PLANS
+        const deleteAllStockPlans = await prisma.stockPlan.deleteMany()
+        console.log(`Deleted ${deleteAllStockPlans.count} stock plans`)
+
+        // VALUATIONS
+        const deleteAllValuations = await prisma.valuations.deleteMany()
+        console.log(`Deleted ${deleteAllValuations.count} valuations`)
+
+        // VESTING TERMS
+        const deleteAllVestingTerms = await prisma.vestingTerms.deleteMany()
+        console.log(`Deleted ${deleteAllVestingTerms.count} vesting terms`)
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
