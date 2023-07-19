5c51817a2adf42fa8fea34390afa08d3ec9a7ae2
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-07-19 14:20:31 -0400

    Import and use Ownable from OpenZeppelin

--- chain/src/Issuer.sol
+++ chain/src/Issuer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract CapTableIssuer is Ownable {
     struct Issuer {
