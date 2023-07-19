63126f8bad0c980f13343e3e23d11fafa1586eab
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-07-19 14:11:20 -0400

    Test new Issuer and cap table contract

---
+++ chain/src/Issuer.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CapTableIssuer is Ownable {
+    struct Issuer {
+        string id;
+        string legalName;
+        string initialSharesAuthorized;
+    }
+
+    Issuer public issuer;
+
+    event IssuerCreated(string id, string legalName, string initialSharesAuthorized);
+    event LegalNameUpdated(string oldLegalName, string newLegalName);
+
+    constructor(string memory id, string memory legalName, string memory initialSharesAuthorized) {
+        issuer = Issuer(id, legalName, initialSharesAuthorized);
+        emit IssuerCreated(id, legalName, initialSharesAuthorized);
+    }
+
+    function getIssuer() public view returns (string memory, string memory, string memory) {
+        return (issuer.id, issuer.legalName, issuer.initialSharesAuthorized);
+    }
+
+    function updateLegalName(string memory legalName) public onlyOwner {
+        string memory oldLegalName = issuer.legalName;
+        issuer.legalName = legalName;
+        emit LegalNameUpdated(oldLegalName, legalName);
+    }
+}
