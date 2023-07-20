ea83fc07d29b8f1ebb09f1d3c55db54924b1b754
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-07-20 14:16:48 -0400

    Rename contract test

---
+++ chain/test/CapTable.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/CapTable.sol";
+
+contract CapTableTest is Test {
+    CapTable public capTable;
+
+    function setUp() public {
+        capTable = new CapTable("123-123-123", "Test Issuer", "10000000");
+    }
+
+    function testUpdateLegalName() public {
+        capTable.updateLegalName("Test Issuer Updated");
+        (, string memory legalName, ) = capTable.getIssuer();
+        assertEq(legalName, "Test Issuer Updated");
+    }
+}
