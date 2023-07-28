df849d1f412886a9103437c933884420e3f4be20
Victor Mimo <victormimoc@gmail.com>
2023-07-27 21:55:04 -0300

    separating forge deployment script between goerli and local

--- chain/script/CapTable.s.sol
+++ chain/script/CapTable.s.sol
@@ -6,16 +6,29 @@ import "forge-std/console.sol";
 
 import "../src/CapTable.sol";
 
-contract CapTableScript is Script {
+contract CapTableDeployLocalScript is Script {
     function setUp() public {}
 
     function run() public {
-        //uint256 deployerPrivateKeyFakeAccount = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKeyFakeAccount = vm.envUint("PRIVATE_KEY_FAKE_ACCOUNT");
+
+        vm.startBroadcast(deployerPrivateKeyFakeAccount);
+
+        CapTable capTable = new CapTable("1111-1111-1111", "Winston Inc.", "10000000");
+
+        vm.stopBroadcast();
+    }
+}
+
+contract CapTableDeployOptimismGoerli is Script {
+    function setUp() public {}
+
+    function run() public {
         uint256 deployerPrivateKeyPoetTest = vm.envUint("PRIVATE_KEY_POET_TEST");
 
         vm.startBroadcast(deployerPrivateKeyPoetTest);
 
-        CapTable capTable = new CapTable("1111-1111-1111", "Justice League", "10000000");
+        CapTable capTable = new CapTable("1111-1111-1111", "Winston Inc.", "10000000");
 
         vm.stopBroadcast();
     }
