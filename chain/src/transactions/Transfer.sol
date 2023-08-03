f352d145d76c21fe56ec96db2f81d86087b9f974
Victor Mimo <victormimoc@gmail.com>
2023-08-03 14:05:13 -0300

    Completing transfer transactions to be complete with OCF

--- chain/src/transactions/Transfer.sol
+++ chain/src/transactions/Transfer.sol
@@ -1,49 +1,61 @@
-pragma solidity ^0.8.3;
-
-// enum {
-//     TX_CONVERTIBLE_TRANSFER
-// }
-// so on.
+pragma solidity ^0.8.19;
 
 contract StockTransferTX {
+    // @dev are unique identifiers in solidity are bytes32?
+    // date fields are going to use block timestamp
     struct StockTransfer {
         string id;
         string object_type;
-        uint256 quantity;
+        string quantity;
+        string[] comments; // optional
         string security_id;
-        string balance_security_id;
+        string consideration_text; // optional
+        string balance_security_id; // optional
         string[] resulting_security_ids;
     }
 
     StockTransfer public stockTransfer;
 
     event StockTransferCreated(
-        string indexed id,
-        string indexed security_id,
+        string id,
         string indexed object_type,
-        uint256  quantity,
+        string quantity,
+        string[] comments,
+        string indexed security_id,
+        string consideration_text,
         string balance_security_id,
         string[] resulting_security_ids
     );
 
    constructor(
-        string memory _id,
-        string memory _object_type,
-        uint256 _quantity,
-        string memory _security_id,
-        string memory _balance_security_id,
-        string[] memory _resulting_security_ids
+        string memory id,        
+        string memory object_type,
+        string memory quantity,
+        string[] memory comments,
+        string memory security_id,
+        string memory consideration_text,
+        string memory balance_security_id,
+        string[] memory resulting_security_ids
     ) {
         stockTransfer = StockTransfer(
-            _id,
-            _object_type,
-            _quantity,
-            _security_id,
-            _balance_security_id,
-            _resulting_security_ids
+            id,
+            object_type,
+            quantity,
+            comments,
+            security_id,
+            consideration_text,
+            balance_security_id,
+            resulting_security_ids
         );
 
-        emit StockTransferCreated(_id, _security_id, _object_type, _quantity, _balance_security_id, _resulting_security_ids);
-
+        emit StockTransferCreated(
+            id,
+            object_type,
+            quantity,
+            comments,
+            security_id,
+            consideration_text,
+            balance_security_id,
+            resulting_security_ids);
     }
 }
