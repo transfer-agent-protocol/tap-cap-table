1a939884e83532526ecb2d9773c5246b28b42433
Victor Mimo <victormimoc@gmail.com>
2023-08-03 17:40:40 -0300

    refactoring stock transfer transaction

--- chain/src/transactions/Transfer.sol
+++ chain/src/transactions/Transfer.sol
@@ -1,61 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import { StockTransfer } from "../lib/Structs.sol";
+
 contract StockTransferTX {
-    // @dev are unique identifiers in solidity are bytes32?
-    // date fields are going to use block timestamp
-    struct StockTransfer {
-        string id;
-        string object_type;
-        string quantity;
-        string[] comments; // optional
-        string security_id;
-        string consideration_text; // optional
-        string balance_security_id; // optional
-        string[] resulting_security_ids;
-    }
+    StockTransfer private stockTransfer;
 
-    StockTransfer public stockTransfer;
+    event StockTransferCreated(StockTransfer stock);
 
-    event StockTransferCreated(
-        string id,
-        string indexed object_type,
-        string quantity,
-        string[] comments,
-        string indexed security_id,
-        string consideration_text,
-        string balance_security_id,
-        string[] resulting_security_ids
-    );
-
-   constructor(
-        string memory id,        
-        string memory object_type,
-        string memory quantity,
-        string[] memory comments,
-        string memory security_id,
-        string memory consideration_text,
-        string memory balance_security_id,
-        string[] memory resulting_security_ids
-    ) {
-        stockTransfer = StockTransfer(
-            id,
-            object_type,
-            quantity,
-            comments,
-            security_id,
-            consideration_text,
-            balance_security_id,
-            resulting_security_ids
-        );
-
-        emit StockTransferCreated(
-            id,
-            object_type,
-            quantity,
-            comments,
-            security_id,
-            consideration_text,
-            balance_security_id,
-            resulting_security_ids);
+    constructor(StockTransfer memory _stockTransfer) {
+        stockTransfer = _stockTransfer;
+        emit StockTransferCreated(_stockTransfer);
     }
 }
+
