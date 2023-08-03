395da63779ce292fd34e4db26a424dbd51cb7345
Victor Mimo <victormimoc@gmail.com>
2023-08-03 14:04:59 -0300

    Completing issuance transactions to be complete with OCF
    Co-authored-by: Alex Palmer <ThatAlexPalmer@users.noreply.github.com>

--- chain/src/transactions/Issuance.sol
+++ chain/src/transactions/Issuance.sol
@@ -1,46 +1,106 @@
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.19;
 
 contract StockIssuanceTX {
+    // @dev are unique identifiers in solidity are bytes32?
+    // date fields are going to use block timestamp
     struct StockIssuance {
         string id;
         string object_type;
-        string stock_class_id;
-        int share_price;
-        uint256 quantity;
-        string security_id;
-        string stakeholder_id;
+        string stock_class_id; 
+        string stock_plan_id; // Optional
+        string share_price; // OCF Monetary (USD is all that matters). Amount is  Numeric: Fixed-point string representation of a number (up to 10 decimal places supported)
+        string quantity; // Numeric: Fixed-point string representation of a number (up to 10 decimal places supported)
+        string vesting_terms_id; // Optional
+        string cost_basis; // OCF Monetary (USD is all that matters). Amount is  Numeric: Fixed-point string representation of a number (up to 10 decimal places supported)
+        string[] stock_legend_ids; // Optional
+        string issuance_type; // Optional for special types (["RSA", "FOUNDERS_STOCK"],)
+        string[] comments; // Optional
+        string security_id; 
+        string stakeholder_id; 
+        string board_approval_date; // Optional
+        string stockholder_approval_date; // Optional
+        string consideration_text; // Optional
+        string[] security_law_exemptions; // Unclear 
     }
 
     StockIssuance public stockIssuance;
 
     event StockIssuanceCreated(
         string id,
-        string indexed stakeholder_id,
-        string indexed security_id,
         string indexed object_type,
         string stock_class_id,
-        uint256 quantity
+        string stock_plan_id,
+        string share_price,
+        string quantity,
+        string vesting_terms_id,
+        string cost_basis,
+        string[] stock_legend_ids,
+        string issuance_type,
+        string[] comments,
+        string indexed security_id,
+        string indexed stakeholder_id,
+        string board_approval_date,
+        string stockholder_approval_date,
+        string consideration_text,
+        string[] security_law_exemptions
     );
 
    constructor(
-        string memory _id,
-        string memory _object_type,
-        string memory _stock_class_id,
-        int  _share_price,
-        uint256 _quantity,
-        string memory _security_id,
-        string memory _stakeholder_id
+        string memory id,
+        string memory object_type,
+        string memory stock_class_id,
+        string memory stock_plan_id,
+        string memory share_price,
+        string memory quantity,
+        string memory vesting_terms_id,
+        string memory cost_basis,
+        string[] memory stock_legend_ids,
+        string memory issuance_type,
+        string[] memory comments,
+        string memory security_id,
+        string memory stakeholder_id,
+        string memory board_approval_date,
+        string memory stockholder_approval_date,
+        string memory consideration_text,
+        string[] memory security_law_exemptions
     ) {
         stockIssuance = StockIssuance(
-            _id,
-            _object_type,
-            _stock_class_id,
-            _share_price,
-            _quantity,
-            _security_id,
-            _stakeholder_id
+            id,
+            object_type,
+            stock_class_id,
+            stock_plan_id,
+            share_price,
+            quantity,
+            vesting_terms_id,
+            cost_basis,
+            stock_legend_ids,
+            issuance_type,
+            comments,
+            security_id,
+            stakeholder_id,
+            board_approval_date,
+            stockholder_approval_date,
+            consideration_text,
+            security_law_exemptions
         );
 
-        emit StockIssuanceCreated(_id, _stakeholder_id, _security_id, _object_type, _stock_class_id, _quantity);
+        emit StockIssuanceCreated(
+            id,
+            object_type,
+            stock_class_id,
+            stock_plan_id,
+            share_price,
+            quantity,
+            vesting_terms_id,
+            cost_basis,
+            stock_legend_ids,
+            issuance_type,
+            comments,
+            security_id,
+            stakeholder_id,
+            board_approval_date,
+            stockholder_approval_date,
+            consideration_text,
+            security_law_exemptions);
     }
 }
