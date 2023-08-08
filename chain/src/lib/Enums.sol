b818e180cb3665e6129a7ab62fc5b2b0e9c84fcb
Victor Mimo <victormimoc@gmail.com>
2023-08-08 10:00:12 -0300

    Creating enums

---
+++ chain/src/lib/Enums.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+enum IssuanceTX {
+    TX_STOCK_ISSUANCE,
+}
+
+enum TransferTX {
+    TX_STOCK_TRANSFER,
+}
+
+enum StockClassType {
+    COMMON,
+    PREFERRED
+}
