2f8251f7c0596bc3089214d18184b65089b8e225
Victor Mimo <victormimoc@gmail.com>
2023-08-08 11:58:45 -0300

    fixing typos

--- chain/src/lib/Enums.sol
+++ chain/src/lib/Enums.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.19;
 
 enum IssuanceTX {
-    TX_STOCK_ISSUANCE,
+    TX_STOCK_ISSUANCE
 }
 
 enum TransferTX {
-    TX_STOCK_TRANSFER,
+    TX_STOCK_TRANSFER
 }
 
 enum StockClassType {
