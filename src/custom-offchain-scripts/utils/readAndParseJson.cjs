06756d0ce98011945439419fefc373688e83c98d
Victor Mimo <victormimoc@gmail.com>
2023-08-09 10:29:56 -0300

    updating path to reflect src

--- src/custom-offchain-scripts/utils/readAndParseJson.cjs
+++ src/custom-offchain-scripts/utils/readAndParseJson.cjs
@@ -1,8 +1,10 @@
 const fs = require("fs").promises;
 const path = require("path");
 
+const ROOT_PATH = "./src/custom-offchain-scripts/samples/";
+
 async function readAndParseJSON(inputPath, notRealCompany) {
-    const dataPath = path.join(`./custom-offchain-scripts/samples/${notRealCompany}`, inputPath);
+    const dataPath = path.join(ROOT_PATH + notRealCompany, inputPath);
     try {
         const data = await fs.readFile(dataPath, "utf8");
         const jsonData = JSON.parse(data);
