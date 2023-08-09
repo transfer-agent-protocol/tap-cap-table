b13626b581bef074e9d35e8263706273048dae02
Victor Mimo <victormimoc@gmail.com>
2023-08-09 11:19:12 -0300

    route to validate and add not poet

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import addNotPoetToDB from "./custom-offchain-scripts/seedNotPoet.js";
+import { exec } from "child_process";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -9,6 +11,26 @@ app.get("/", async (req, res) => {
     res.send(`Hello World!`);
 });
 
+app.get("/add-not-poet", async (req, res) => {
+    exec("yarn validate-not-poet-files", async (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        // console.error(`stderr: ${stderr}`);
+
+        await addNotPoetToDB(prisma);
+
+        console.log("Success");
+
+        res.send("success");
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+ 
+
+// await prisma.$disconnect()
