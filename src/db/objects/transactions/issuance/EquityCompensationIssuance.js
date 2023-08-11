13588d84e27ae3c5d1570b9934764349216b01ab
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-08-11 14:16:00 -0400

    Add issuance transaction models

---
+++ src/db/objects/transactions/issuance/EquityCompensationIssuance.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { v4 as uuid } from "uuid";
+
+const EquityCompensationType = ["TX_PLAN_SECURITY_ISSUANCE", "TX_EQUITY_COMPENSATION_ISSUANCE"];
+
+const EquityCompensationIssuanceSchema = new mongoose.Schema({
+    id: { type: String, default: () => uuid() },
+    object_type: { type: String, enum: EquityCompensationType },
+    stock_plan_id: String,
+    stock_class_id: String,
+    compensation_type: String,
+    option_grant_type: String,
+    quantity: String,
+    exercise_price: {},
+    base_price: {},
+    early_exercisable: Boolean,
+    vesting_terms_id: String,
+    expiration_date: String,
+    termination_exercise_windows: {},
+    comments: [String],
+    security_id: String,
+    date: String,
+    custom_id: String,
+    stakeholder_id: String,
+    board_approval_date: String,
+    stockholder_approval_date: String,
+    consideration_text: String,
+    security_law_exemptions: [{}],
+});
+
+const EquityCompensationIssuance = mongoose.model("EquityCompensationIssuance", EquityCompensationIssuanceSchema);
+
+export default EquityCompensationIssuance;
