50d2ee33dd2d277f9f65947286fed7365269bc81
Victor Mimo <victormimoc@gmail.com>
2023-08-11 13:18:38 -0300

    Making issue by ta verbose

--- chain/src/lib/TransactionHelper.sol
+++ chain/src/lib/TransactionHelper.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import { StockIssuance, StockTransfer, ShareNumbersIssued } from "./Structs.sol";
 
 library TransactionHelper {
-
     function generateDeterministicUniqueID(bytes16 stakeholderId) public view returns (bytes16) {
         bytes16 deterministicValue = bytes16(keccak256(abi.encodePacked(stakeholderId, block.timestamp, block.prevrandao)));
         return deterministicValue;
@@ -14,51 +13,60 @@ library TransactionHelper {
         - one created during transfers (createStockIssuanceStructForTransfer) where less info is needed.
         - one created one time by the TA for an major issuance event (createStockIssuanceStructByTA)  , like a new investor with the company or employee
     */
-    function createStockIssuanceStructForTransfer(
-        bytes16 stakeholderId,
-        uint256 quantity,
-        int sharePrice,
-        bytes16 stockClassId
-    ) internal view returns (StockIssuance memory issuance) {
-        ShareNumbersIssued memory share_numbers_issued; // if not instatiated it defaults to 0 for both values
-        
-        // bytes16 id = hex"f02b7c91e70947f9a748c2a2908af654";
 
+    function createStockIssuanceStructByTA(
+        bytes16 stockClassId,
+        bytes16 stockPlanId,
+        ShareNumbersIssued memory shareNumbersIssued,
+        uint256 sharePrice,
+        uint256 quantity,
+        bytes16 vestingTermsId,
+        uint256 costBasis,
+        bytes16[] memory stockLegendIds,
+        string memory issuanceType,
+        string[] memory comments,
+        string memory customId,
+        bytes16 stakeholderId,
+        string memory boardApprovalDate,
+        string memory stockholderApprovalDate,
+        string memory considerationText,
+        string[] memory securityLawExemptions
+    ) internal view returns (StockIssuance memory issuance) {
         bytes16 id = generateDeterministicUniqueID(stakeholderId);
+        bytes16 secId = generateDeterministicUniqueID(stockClassId);
 
         return
             StockIssuance(
-                id, // ID -> needs to be a UUID
-                "TX_STOCK_ISSUANCE", // Transaction type
-                stockClassId, // Stock class ID
-                "", // Stock plan ID (optional)
-                share_numbers_issued, // Share numbers issued (optional)
-                sharePrice, // Share price
-                quantity, // Quantity
-                "", // Vesting terms ID (optional)
-                "", // Cost basis (optional)
-                new bytes16[](0), // Stock legend IDs (optional)
-                "", // Issuance type (optional)
-                new string[](0), // Comments
-                "sec-1", // Security ID -> needs to be a UUID
-                "", // Custom ID (optional)
-                stakeholderId, // Stakeholder ID
-                "", // Board approval date (optional)
-                "", // Stockholder approval date (optional)
-                "", // Consideration text (optional)
-                new string[](0) // Security law exemptions (optional)?
+                id,
+                "TX_STOCK_ISSUANCE",
+                stockClassId,
+                stockPlanId,
+                shareNumbersIssued,
+                sharePrice,
+                quantity,
+                vestingTermsId,
+                costBasis,
+                stockLegendIds,
+                issuanceType,
+                comments,
+                secId,
+                customId,
+                stakeholderId,
+                boardApprovalDate,
+                stockholderApprovalDate,
+                considerationText,
+                securityLawExemptions
             );
     }
 
-    function createStockIssuanceStructByTA(
+    function createStockIssuanceStructForTransfer(
         bytes16 stakeholderId,
         uint256 quantity,
-        int sharePrice,
+        uint256 sharePrice,
         bytes16 stockClassId
     ) internal view returns (StockIssuance memory issuance) {
-        
         ShareNumbersIssued memory share_numbers_issued; // if not instatiated it defaults to 0 for both values
-        
+
         // Temporary placeholders for UUIDs
         bytes16 id = generateDeterministicUniqueID(stakeholderId);
         bytes16 securityId = generateDeterministicUniqueID(stockClassId);
@@ -73,7 +81,7 @@ library TransactionHelper {
                 sharePrice, // Share price
                 quantity, // Quantity
                 "", // Vesting terms ID (optional)
-                "", // Cost basis (optional)
+                0e10, // Cost basis (optional)
                 new bytes16[](0), // Stock legend IDs (optional)
                 "", // Issuance type (optional)
                 new string[](0), // Comments
@@ -95,7 +103,7 @@ library TransactionHelper {
     ) internal view returns (StockTransfer memory transfer) {
         bytes16[] memory resultingSecurityIds = new bytes16[](1);
         resultingSecurityIds[0] = resulting_security_id;
-        
+
         // bytes16 id = hex"f02b7c91e70947f9a748c2a2908af657";
         bytes16 id = generateDeterministicUniqueID(security_id);
 
@@ -111,5 +119,4 @@ library TransactionHelper {
                 resultingSecurityIds
             );
     }
-
 }
