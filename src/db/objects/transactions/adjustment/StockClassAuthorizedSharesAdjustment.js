101f6f9769a3d59f589439f79e60b194707ec666
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-08-11 15:06:59 -0400

    Add adjustment

---
+++ src/db/objects/transactions/adjustment/StockClassAuthorizedSharesAdjustment.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { v4 as uuid } from "uuid";
+
+const StockClassAuthorizedSharesAdjustmentSchema = new mongoose.Schema({
+    id: { type: String, default: () => uuid() },
+    object_type: { type: String, default: "TX_STOCK_CLASS_AUTHORIZED_SHARES_ADJUSTMENT" },
+    comments: [String],
+    date: String,
+    stock_class_id: String,
+    new_shares_authorized: String,
+    board_approval_date: String,
+    stockholder_approval_date: String,
+});
+
+const StockClassAuthorizedSharesAdjustment = mongoose.model("StockClassAuthorizedSharesAdjustment", StockClassAuthorizedSharesAdjustmentSchema);
+
+export default StockClassAuthorizedSharesAdjustment;
