989eb6f91b75c4a052f6d949569bdd2f25624dbf
Victor Mimo <victormimoc@gmail.com>
2023-08-11 13:19:28 -0300

    putting local set up back until bug is fixed for cjs

--- src/custom-chain-scripts/capTable.cjs
+++ src/custom-chain-scripts/capTable.cjs
@@ -1,8 +1,50 @@
 const { v4: uuid } = require("uuid");
-
-const { localSetup, optimismGoerliSetup } = require("./chainSetup.js");
 const { ethers, utils } = require("ethers");
+const dotenv = require("dotenv").config();
+const CAP_TABLE = require("../../chain/out/CapTable.sol/CapTable.json");
+const CAP_TABLE_ABI = CAP_TABLE.abi;
 
+async function localSetup() {
+    // if deployed using forge script
+    //const CONTRACT_ADDRESS_LOCAL = require("../chain/broadcast/CapTable.s.sol/31337/run-latest.json").transactions[0].contractAddress;
+    const CONTRACT_ADDRESS_LOCAL = "0x5fbdb2315678afecb367f032d93f642f64180aa3"; // fill in from capTableFactory
+
+    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_FAKE_ACCOUNT;
+
+    const customNetwork = {
+        chainId: 31337,
+        name: "local",
+    };
+
+    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545", customNetwork);
+    const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
+    const contract = new ethers.Contract(CONTRACT_ADDRESS_LOCAL, CAP_TABLE_ABI, wallet);
+
+    return { contract, provider };
+}
+
+async function optimismGoerliSetup() {
+    // if deployed using forge script
+    // const CONTRACT_ADDRESS_OPTIMISM_GOERLI = require("../chain/broadcast/CapTable.s.sol/420/run-latest.json").transactions[0].contractAddress;
+    const CONTRACT_ADDRESS_OPTIMISM_GOERLI = "0x027A280A63376308658A571ac2DB5D612bA77912";
+    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_POET_TEST;
+
+    const provider = new ethers.providers.JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_URL);
+    const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
+    const contract = new ethers.Contract(CONTRACT_ADDRESS_OPTIMISM_GOERLI, CAP_TABLE_ABI, wallet);
+
+    return contract;
+}
+
+// Convert a price to a BigNumber
+function toScaledBigNumber(price) {
+    return ethers.BigNumber.from(Math.round(price * 1e10).toString());
+}
+
+// Convert a BigNumber back to a decimal price
+function toDecimalPrice(scaledPriceBigNumber) {
+    return parseFloat(scaledPriceBigNumber / 1e10).toString();
+}
 
 async function createAndDisplayStakeholder(contract) {
     const stakeholderId = uuid();
@@ -29,20 +71,23 @@ async function createAndDisplayStockClass(contract) {
     try {
         const stockClassId = uuid();
         const stockClassIdBytes16 = utils.hexlify(utils.arrayify("0x" + stockClassId.replace(/-/g, "")));
+        const sharePrice = 0.987654321;
+        const scaledSharePrice = toScaledBigNumber(sharePrice);
+        console.log("scaledSharePrice", scaledSharePrice.toString());
 
-        const newStockClass = await contract.createStockClass(stockClassIdBytes16, "COMMON", 100, 4000000);
+        const newStockClass = await contract.createStockClass(stockClassIdBytes16, "COMMON", scaledSharePrice, 4000000);
         await newStockClass.wait();
         const stockClassAdded = await contract.getStockClassById(stockClassIdBytes16);
         console.log("--- Stock Class for Existing ID ---");
         console.log("Getting new stock class:");
         console.log("ID:", stockClassAdded[0]);
         console.log("Type:", stockClassAdded[1]);
-        console.log("Price Per Share:", ethers.utils.formatUnits(stockClassAdded[2], 6));
+        console.log("Price Per Share:", toDecimalPrice(stockClassAdded[2].toString()));
         console.log("Initial Shares Authorized:", stockClassAdded[3].toString());
 
         return stockClassIdBytes16;
     } catch (error) {
-        console.error("Error encountered:", error.error.reason);
+        console.error("Error encountered:", error);
     }
 }
 
@@ -132,14 +177,14 @@ async function main({ chain }) {
     }
     const transferorId = "0x1636c898717741fbaa72f735622cad35";
     const transfereeId = "0xb3174fbc904c495585690a1002351ee3";
-    const stockClassId = "0xe6a02695fdf7479bb2e1a362e9cdfc69";
+    //const stockClassId = "0xe6a02695fdf7479bb2e1a362e9cdfc69";
 
     //await issuerTest(_contract);
     // await displayIssuer(contract);
-    //const id = await createAndDisplayStakeholder(_contract);
-    //const stockClassId = await createAndDisplayStockClass(_contract);
-    //await issueStakeholderStock(_contract, id, stockClassId);
-    await transferOwnership(_contract, transferorId, transfereeId, stockClassId);
+    const id = await createAndDisplayStakeholder(_contract);
+    const stockClassId = await createAndDisplayStockClass(_contract);
+    await issueStakeholderStock(_contract, id, stockClassId);
+    //await transferOwnership(_contract, transferorId, transfereeId, stockClassId);
     // await totalNumberOfStakeholders(contract);
 }
 
