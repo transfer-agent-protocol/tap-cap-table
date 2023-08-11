4bf7c43b0e9be37cce6b5d5204d1d2df53e93b0d
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-08-11 14:51:10 -0400

    Update reissuance and repurchase

---
+++ src/db/objects/transactions/repurchase/StockRepurchase.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { v4 as uuid } from "uuid";
+
+const StockRepurchaseSchema = new mongoose.Schema({
+    id: { type: String, default: () => uuid() },
+    object_type: { type: String, default: "TX_STOCK_REPURCHASE" },
+    comments: [String],
+    security_id: String,
+    date: String,
+    price: { type: Map, of: String }, // or Json
+    quantity: String,
+    consideration_text: String,
+    balance_security_id: String,
+});
+
+const StockRepurchase = mongoose.model("StockRepurchase", StockRepurchaseSchema);
+
+export default StockRepurchase;
