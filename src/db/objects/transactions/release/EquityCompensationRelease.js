45dcdb67c1e2adb0e95c0794ba0ccd8661dbbad2
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-08-11 14:45:29 -0400

    Update release

---
+++ src/db/objects/transactions/release/EquityCompensationRelease.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { v4 as uuid } from "uuid";
+
+const EquityCompensationReleaseType = ["TX_PLAN_SECURITY_RELEASE", "TX_EQUITY_COMPENSATION_RELEASE"];
+
+const EquityCompensationReleaseSchema = new mongoose.Schema({
+    id: { type: String, default: () => uuid() },
+    object_type: { type: String, enum: EquityCompensationReleaseType },
+    comments: [String],
+    security_id: String,
+    date: String,
+    settlement_date: String,
+    release_price: mongoose.Schema.Types.Mixed,
+    quantity: String,
+    consideration_text: String,
+    resulting_security_ids: [String],
+});
+
+const EquityCompensationRelease = mongoose.model("EquityCompensationRelease", EquityCompensationReleaseSchema);
+
+export default EquityCompensationRelease;
---
+++ src/db/objects/transactions/release/PlanSecurityRelease.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { v4 as uuid } from "uuid";
+
+const PlanSecurityReleaseSchema = new mongoose.Schema({
+    id: { type: String, default: () => uuid() },
+    object_type: { type: String, default: "TX_PLAN_SECURITY_RELEASE" },
+    comments: [String],
+    security_id: String,
+    date: String,
+    settlement_date: String,
+    release_price: mongoose.Schema.Types.Mixed,
+    quantity: String,
+    consideration_text: String,
+    resulting_security_ids: [String],
+});
+
+const PlanSecurityRelease = mongoose.model("PlanSecurityRelease", PlanSecurityReleaseSchema);
+
+export default PlanSecurityRelease;
