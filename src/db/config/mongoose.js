5ae0c251b5bdbed4be76b4f3cdacc0a3a28e058a
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-08-11 12:54:17 -0400

    Setup mongoose config and improve file structure

---
+++ src/db/config/mongoose.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(DATABASE_URL);
+        console.log("Mongo connected succesfully");
+    } catch (error) {
+        console.error("Error connecting to Mongo", error.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
+
+export default connectDB;
