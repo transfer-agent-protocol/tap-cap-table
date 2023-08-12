ee7497d9ad114c601fffb5b0d5474ff62b6b2cb3
Victor Mimo <victormimoc@gmail.com>
2023-08-12 17:13:04 -0300

    adding more stakeholder route scaffod

--- src/routes/stakeholder.js
+++ src/routes/stakeholder.js
@@ -6,4 +6,12 @@ stakeholder.get("/", async (req, res) => {
     res.send(`Hello stakeholder!`);
 });
 
+stakeholder.get("/:id", async (req, res) => {
+    const { id } = req.params;
+});
+
+stakeholder.post("/create", async (req, res) => {});
+
+stakeholder.post("/reflect-onchain", async (req, res) => {});
+
 export default stakeholder;
