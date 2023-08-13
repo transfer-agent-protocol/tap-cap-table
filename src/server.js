8e4d3db130cb6fedd7e77d3056828a8716d11a76
Victor Mimo <victormimoc@gmail.com>
2023-08-13 18:26:46 -0300

    adding middleware for contract

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ config();
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import startOnchainListeners from "./custom-chain-scripts/transactionListener.js";
+import getContractInstance from "./custom-chain-scripts/getContractInstances.js";
 
 // Routes
-import mainRoutes from "./routes/main.js";
+import mainRoutes from "./routes/index.js";
 import issuerRoutes from "./routes/issuer.js";
 import stakeholderRoutes from "./routes/stakeholder.js";
 import stockClassRoutes from "./routes/stockClass.js";
@@ -16,19 +17,26 @@ const app = express();
 const PORT = 3000;
 const CHAIN = "local"; // change this to prod or env style variable
 
-// middlewares
-app.use(urlencoded({ limit: "50mb", extended: true }));
-app.use(json({ limit: "50mb" }));
-app.enable("trust proxy");
+// Middlewares
+const contractMiddleware = async (req, res, next) => {
+    req.contract = await getContractInstance(CHAIN);
+    next();
+};
+
 app.use((req, res, next) => {
     req.prisma = prisma;
     req.chain = CHAIN;
     next();
 });
+
+app.use(urlencoded({ limit: "50mb", extended: true }));
+app.use(json({ limit: "50mb" }));
+app.enable("trust proxy");
+
 app.use("/", mainRoutes);
-app.use("/issuer", issuerRoutes);
-app.use("/stakeholder", stakeholderRoutes);
-app.use("/stock-class", stockClassRoutes);
+app.use("/issuer", contractMiddleware, issuerRoutes);
+app.use("/stakeholder", contractMiddleware, stakeholderRoutes);
+app.use("/stock-class", contractMiddleware, stockClassRoutes);
 
 app.listen(PORT, async () => {
     console.log(`🚀  Server successfully launched. Access at: http://localhost:${PORT}`);
