bc5b6d0b46295c734bf55259e0df693ad0177fb2
Victor Mimo <victormimoc@gmail.com>
2023-08-14 14:07:28 -0300

    foung bug in converting bytes to UUID, putting placeholder WIP

--- src/utils/convertUUID.js
+++ src/utils/convertUUID.js
@@ -1,7 +1,7 @@
 import { utils } from "ethers";
 
 function convertToUUID(uuidBytes16) {
-    let uuidWithoutDashes = utils.stripZeros(uuidBytes16).substr(2); // removes the '0x' prefix and leading zeros
+    let uuidWithoutDashes = uuidBytes16.substring(2); // removes the '0x' prefix
     let uuid = [
         uuidWithoutDashes.slice(0, 8),
         "-",
@@ -16,20 +16,25 @@ function convertToUUID(uuidBytes16) {
     return uuid;
 }
 
-function convertBytes16ToUUID(object) {
-    let newObject;
-    Object.keys(object).forEach((field) => {
-        if (object[field].includes("0x")) {
-            newObject[field] = convertToUUID(object[field]);
-        } else if (Array.isArray(object[field]) && object[field].length > 0) {
-            if (object[field][0].includes("0x")) {
-                newObject[field] = object[field].map(convertUUID);
+// WIP: his function doesn't work for arrays
+function convertBytes16ToUUID(arr) {
+    let newArr;
+    if (!Array.isArray(arr)) {
+        return convertToUUID(object);
+    } else {
+        arr.forEach((field) => {
+            if (field.includes("0x")) {
+                newObject[field] = convertToUUID(object[field]);
+            } else if (Array.isArray(object[field]) && object[field].length > 0) {
+                if (object[field][0].includes("0x")) {
+                    newObject[field] = object[field].map(convertUUID);
+                }
+            } else {
+                newObject[field] = object[field];
             }
-        } else {
-            newObject[field] = object[field];
-        }
-    });
-    return newObject;
+        });
+        return newObject;
+    }
 }
 
 function convertUUIDToBytes16(uuid) {
