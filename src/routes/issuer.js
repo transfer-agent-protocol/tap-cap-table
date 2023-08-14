f5a664a4042172b0443158ec48e2672bd49adf53
Victor Mimo <victormimoc@gmail.com>
2023-08-14 14:03:31 -0300

    get issuer route

--- src/routes/issuer.js
+++ src/routes/issuer.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { convertBytes16ToUUID } from "../utils/convertUUID.js";
 
 const issuer = Router();
 
-issuer.get("/", async (req, res) => {
-    res.send(`Hello issuer!`);
+issuer.get("/fetch", async (req, res) => {
+    const { contract } = req;
+    const issuer = await contract.issuer();
+    const issuerUUID = convertBytes16ToUUID(issuer.id);
+    console.log("Issuer", issuer);
+    const issuerNew = {
+        id: issuerUUID,
+        legal_name: issuer.legal_name,
+    };
+    res.status(200).send(issuerNew);
 });
 
 export default issuer;
