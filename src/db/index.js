13b9c2a85d17a8d35f609bf69a51375bfd67ccde
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-08-14 18:26:04 -0400

    Delete index and seed

--- src/db/index.js
+++
@@ -1,8 +0,0 @@
-import connectDB from "./config/mongoose.js";
-import addNotPoetToDB from "./operations/seed.js"; // Import the seed function
-
-connectDB();
-
-addNotPoetToDB() // Call the seed function
-    .then(() => console.log("✅ Database seeded successfully"))
-    .catch((err) => console.log("❌ Error seeding database:", err));
