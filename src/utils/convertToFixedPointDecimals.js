56afbc1a0352ebf9d698a4bac7e9976f9f2e2280
Victor Mimo <victormimoc@gmail.com>
2023-08-14 14:06:50 -0300

    extending converting of big numbers to include a many

--- src/utils/convertToFixedPointDecimals.js
+++ src/utils/convertToFixedPointDecimals.js
@@ -1,11 +1,22 @@
+import ethers, { utils } from "ethers";
+
 // Convert a price to a BigNumber
 function toScaledBigNumber(price) {
     return ethers.BigNumber.from(Math.round(price * 1e10).toString());
 }
 
 // Convert a BigNumber back to a decimal price
-function toDecimalPrice(scaledPriceBigNumber) {
-    return parseFloat(scaledPriceBigNumber / 1e10).toString();
+function toDecimal(scaledPriceBigNumber) {
+    if (ethers.BigNumber.isBigNumber(scaledPriceBigNumber)) {
+        const numberString = scaledPriceBigNumber.toString();
+        return parseFloat(numberString / 1e10).toString();
+    } else {
+        return scaledPriceBigNumber;
+    }
 }
 
-export { toScaledBigNumber, toDecimalPrice };
+function convertManyToDecimal(scaledPriceBigNumbers) {
+    return scaledPriceBigNumbers.map(toDecimal);
+}
+
+export { toScaledBigNumber, toDecimal, convertManyToDecimal };
