7cb41f1f5e9e58214815c2fceda4515069df6396
Victor Mimo <victormimoc@gmail.com>
2023-08-15 11:15:13 -0300

    commenting out prisma preparing for merge

--- src/custom-chain-scripts/transactionListener.js
+++ src/custom-chain-scripts/transactionListener.js
@@ -40,30 +40,30 @@ async function startOnchainListeners(chain, prisma) {
 
         //const stockIssuance = convertBytes16ToUUID(stock);
 
-        const createdStockIssuance = await prisma.stockIssuance.create({
-            data: {
-                id: stockIssuance.id,
-                object_type: stockIssuance.object_type,
-                stock_class_id: stockIssuance.stock_class_id,
-                stock_plan_id: stockIssuance.stock_plan_id,
-                share_numbers_issued: stockIssuance.share_numbers_issued.toString(), // OCF structure is [{}], check how it returns
-                share_price: sharePriceOCF,
-                quantity: stock.quantity.toString(),
-                vesting_terms_id: stockIssuance.vesting_terms_id,
-                cost_basis: costBasisOCF,
-                stock_legend_ids: stockIssuance.stock_legend_ids,
-                issuance_type: stockIssuance.issuance_type,
-                comments: stockIssuance.comments,
-                security_id: stockIssuance.security_id,
-                date: dateOCF,
-                custom_id: stockIssuance.custom_id,
-                stakeholder_id: stockIssuance.stakeholder_id,
-                board_approval_date: stockIssuance.board_approval_date,
-                stockholder_approval_date: stockIssuance.stockholder_approval_date,
-                consideration_text: stockIssuance.consideration_text,
-                security_law_exemptions: stockIssuance.security_law_exemptions,
-            },
-        });
+        // const createdStockIssuance = await prisma.stockIssuance.create({
+        //     data: {
+        //         id: stockIssuance.id,
+        //         object_type: stockIssuance.object_type,
+        //         stock_class_id: stockIssuance.stock_class_id,
+        //         stock_plan_id: stockIssuance.stock_plan_id,
+        //         share_numbers_issued: stockIssuance.share_numbers_issued.toString(), // OCF structure is [{}], check how it returns
+        //         share_price: sharePriceOCF,
+        //         quantity: stock.quantity.toString(),
+        //         vesting_terms_id: stockIssuance.vesting_terms_id,
+        //         cost_basis: costBasisOCF,
+        //         stock_legend_ids: stockIssuance.stock_legend_ids,
+        //         issuance_type: stockIssuance.issuance_type,
+        //         comments: stockIssuance.comments,
+        //         security_id: stockIssuance.security_id,
+        //         date: dateOCF,
+        //         custom_id: stockIssuance.custom_id,
+        //         stakeholder_id: stockIssuance.stakeholder_id,
+        //         board_approval_date: stockIssuance.board_approval_date,
+        //         stockholder_approval_date: stockIssuance.stockholder_approval_date,
+        //         consideration_text: stockIssuance.consideration_text,
+        //         security_law_exemptions: stockIssuance.security_law_exemptions,
+        //     },
+        // });
 
         console.log("New Stock Issuance Object Created !", createdStockIssuance);
     });
