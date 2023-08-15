2a148c68828e94658f0df31587ca2512d767cfe8
Victor Mimo <victormimoc@gmail.com>
2023-08-15 12:30:02 -0300

    refactoring to remove prisma

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { promisify } from "util";
 import { exec as originalExec } from "child_process";
-import addNotPoetToDB from "../custom-offchain-scripts/seedNotPoet.js";
 import { convertUUIDToBytes16 } from "../utils/convertUUID.js";
 import deployCapTable from "../custom-chain-scripts/deployCapTable.js";
+import addNotPoetToDB from "../db/scripts/seed.js";
+import { readIssuerById } from "../db/operations/read.js";
 
 const exec = promisify(originalExec);
 
@@ -17,7 +18,6 @@ router.get("/", async (req, res) => {
 // needs to be generalized and importing JSONs needs to be shaped.
 router.post("/add-not-poet-to-db", async (req, res) => {
     // TODO: Input validation
-    const { prisma } = req;
     try {
         console.log("here");
         // Validate and Insert to DB
@@ -25,8 +25,7 @@ router.post("/add-not-poet-to-db", async (req, res) => {
 
         // console.log(`stdout: ${stdout}`);
 
-        await addNotPoetToDB(prisma);
-        await prisma.$disconnect();
+        await addNotPoetToDB();
 
         console.log("Success");
         res.status(200).send("Success");
@@ -37,43 +36,23 @@ router.post("/add-not-poet-to-db", async (req, res) => {
 
 router.post("/mint-cap-table", async (req, res) => {
     // TODO: Input validation
-    const { prisma, chain } = req;
+    const { chain } = req;
     const { issuerId } = req.body;
 
     try {
         // get issuer info
-        const issuer = await prisma.issuer.findUnique({
-            where: {
-                id: issuerId,
-            },
-        });
+        const issuer = await readIssuerById(issuerId);
 
         if (!issuer) {
             res.status(500).send("Issuer not found");
         }
-
         console.log("issuer ", issuer);
-
         const issuerIdBytes16 = convertUUIDToBytes16(issuerId);
-
         console.log("issuer ID bytes32", issuerIdBytes16);
-
         const deployedTo = await deployCapTable(chain, issuerIdBytes16, issuer.legal_name);
 
-        await prisma.issuer.update({
-            where: {
-                id: issuerId,
-            },
-            data: {
-                comments: [`Cap Table has been deployed to ${deployedTo}`],
-            },
-        });
-
-        await prisma.$disconnect();
-
-        console.log("success");
-
-        res.status(200).send("Success");
+        console.log("Minted Cap Table to ", deployedTo);
+        res.status(200).send(deployedTo);
     } catch (error) {
         console.error(`exec error: ${error}`);
     }
