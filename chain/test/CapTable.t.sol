082b6f29a0a59923a45bbe6bd1d907dbdec909f6
Victor Mimo <victormimoc@gmail.com>
2023-08-16 11:07:24 -0300

    setting tests back up, will create a separate test branch

--- chain/test/CapTable.t.sol
+++ chain/test/CapTable.t.sol
@@ -9,76 +9,36 @@ import "../src/CapTable.sol";
 contract CapTableTest is Test {
     CapTable public capTable;
 
-    // function setUp() public {
-    //     capTable = new CapTable("123-123-123", "Test Issuer", "10000000");
-    // }
+    function setUp() public {
+        capTable = new CapTable("123-123-123", "Test Issuer");
+    }
 
-    // /* Helpers */
-    // function convertStringToHash (string memory _str) public pure returns (bytes32) {
-    //     bytes32 hash = keccak256(abi.encodePacked(_str));
-    //     return hash;
-    // }
+    /* Helpers */
+    function convertStringToHash(string memory _str) public pure returns (bytes32) {
+        bytes32 hash = keccak256(abi.encodePacked(_str));
+        return hash;
+    }
 
-    // function createPranksterAndExpectRevert() public {
-    //     address prankster = address(0);
-    //     vm.prank(prankster);
-    //     vm.expectRevert();
-    // }
+    function createPranksterAndExpectRevert() public {
+        address prankster = address(0);
+        vm.prank(prankster);
+        vm.expectRevert();
+    }
 
-    // function createInitialDummyStockClassData () public pure returns (string memory, string memory, uint256, uint256, uint256) {
-    //     string memory expectedId = "1234-1234-1234";
-    //     string memory expectedClassType = "Common";
-    //     uint256 expectedPricePerShare = 100;
-    //     uint256 expectedParValue = 1;
-    //     uint256 expectedInitialSharesAuthorized = 10000000;
+    function createInitialDummyStockClassData() public pure returns (string memory, string memory, uint256, uint256, uint256) {
+        string memory expectedId = "1234-1234-1234";
+        string memory expectedClassType = "Common";
+        uint256 expectedPricePerShare = 100;
+        uint256 expectedParValue = 1;
+        uint256 expectedInitialSharesAuthorized = 10000000;
 
-    //     return (expectedId, expectedClassType, expectedPricePerShare, expectedParValue, expectedInitialSharesAuthorized);
-    // }
+        return (expectedId, expectedClassType, expectedPricePerShare, expectedParValue, expectedInitialSharesAuthorized);
+    }
 
-    // /* Issuer Tests */
-    // function testIssuerCreated() public {
-    //     (string memory id, ,) = capTable.getIssuer();
-
-    //     assertEq(id, "123-123-123", "Error: IDs don't match.");
-    // }
-
-    // function testCannotUpdateLegalNameWithWrongOwner() public {
-    //     capTable.updateLegalName("Plato Inc.");
-
-    //     // set a new address to test that only owner can change legal name
-    //     createPranksterAndExpectRevert();
-
-    //     // should not update legal name
-    //     capTable.updateLegalName("Apple Inc.");
-
-    //     (, string memory legalName, ) = capTable.getIssuer();
-    //     assertEq(convertStringToHash(legalName), convertStringToHash("Plato Inc."), "Legal names do not match, and they should");
-    //     assertNotEq(convertStringToHash(legalName), convertStringToHash("Apple Inc."), "Legal names match, and they shouldn't");
-
-    //     console.log("Legal name is ", legalName);
-    // }
-
-    // function testUpdateLegalNameWithOwner () public {
-    //     capTable.updateLegalName("Apple Inc.");
-    //     (, string memory legalName, ) = capTable.getIssuer();
-    //     console.log("Legal name ", legalName);
-    //     assertEq(convertStringToHash(legalName), convertStringToHash("Apple Inc."), "Legal names do not match, and they should");
-    // }
-
-    // function testCannotUpdateLegalNameToEmptyString() public {
-    //     vm.expectRevert();
-    //     // legal name should not be updated
-    //     capTable.updateLegalName("");
-
-    //     (, string memory legalName, ) = capTable.getIssuer();
-    //     console.log("legal name is ", legalName);
-    //     assertEq(convertStringToHash(legalName), convertStringToHash("Test Issuer"), "Legal names do not match, and they should");
-    // }
-
-    // /* Stakeholder Tests */
+    /* Stakeholder Tests */
     // function testCreateStakeholderWithOwner() public {
-    //     string memory expectedId = "1234-1234-1234";
-    //     capTable.createStakeholder(expectedId);
+    //     bytes16  expectedId = bytes16("0xf47ac10b58cc4372a5670e02b2c3d479");
+    //     capTable.createStakeholder(expectedId, "INDIVIDUAL", "ADVISOR");
     //     string memory actualId = capTable.getStakeholderById(expectedId);
     //     assertEq(actualId, expectedId);
     //     assertNotEq(actualId, "4444-4444-4444");
@@ -108,33 +68,6 @@ contract CapTableTest is Test {
     //     assertEq(totalStakeholdersBefore, totalStakeholdersAfter, "Total number of stakeholders has changed and it shouldn't have");
     // }
 
-    // function testCreateManyStakeholders() public {
-    //     // Creating 5 stakeholders
-    //     capTable.createStakeholder("1111-1111-1111-1111");
-    //     capTable.createStakeholder("2222-2222-2222-2222");
-    //     capTable.createStakeholder("3333-3333-3333-3333");
-    //     capTable.createStakeholder("4444-4444-4444-4444");
-    //     capTable.createStakeholder("5555-5555-5555-5555");
-
-    //     // fetch total number
-    //     uint256 totalStakeholders = capTable.getTotalNumberOfStakeholders();
-
-    //     assertEq(totalStakeholders, 5, "Error: Total number is not 5");
-    //     assertNotEq(totalStakeholders, 4, "Error: Total number is 4");
-    // }
-
-    // function testFakeIdDoesNotReturnStakeholder() public {
-    //     string memory realId = "1111-1111-1111-1111";
-    //     capTable.createStakeholder(realId);
-    //     string memory fetchedRealId = capTable.getStakeholderById(realId);
-
-    //     string memory fakeId = "9999-9999-9999-9999";
-    //     string memory emptyId = capTable.getStakeholderById(fakeId);
-
-    //     assertEq(realId, fetchedRealId, "Error: Real ID does not match fetched ID");
-    //     assertNotEq(fakeId, emptyId, "Error: Fake ID matches empty ID");
-    // }
-
     // function testCannotCreateDuplicateStakeholders() public {
     //     string memory stakeholderId = "1111-1111-1111-1111";
     //     capTable.createStakeholder(stakeholderId);
@@ -151,7 +84,7 @@ contract CapTableTest is Test {
     //     assertEq(totalStakeholdersBefore, totalStakeholdersAfter, "Total number of stakeholders has changed and it shouldn't have");
     // }
 
-    // /* Stock Class Tests */
+    /* Stock Class Tests */
     //  function testCreateStockClassWithOwner() public {
     //      (
     //         string memory expectedId,
