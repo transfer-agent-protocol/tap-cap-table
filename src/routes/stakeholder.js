b13464f7f3fcd4df43cdbae8106f5a2f79785089
Victor Mimo <victormimoc@gmail.com>
2023-08-21 16:28:18 -0300

    addig get routes and wallet routes

--- src/routes/stakeholder.js
+++ src/routes/stakeholder.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
     createAndValidateStakeholder,
     convertAndReflectStakeholderOnchain,
+    addWalletToStakeholder,
+    removeWalletFromStakeholder,
     getStakeholderById,
     getTotalNumberOfStakeholders,
 } from "../db/controllers/stakeholderController.js"; // Importing the controller functions
@@ -12,9 +14,33 @@ stakeholder.get("/", async (req, res) => {
     res.send(`Hello stakeholder!`);
 });
 
-stakeholder.get("/onchain/id/:id", getStakeholderById);
-stakeholder.get("/onchain/total-number", getTotalNumberOfStakeholders);
+stakeholder.get("/id/:id", async (req, res) => {
+    try {
+        const { contract } = req;
+        const { id } = req.params;
 
+        const { stakeholderId, type, role } = await getStakeholderById(contract, id);
+
+        res.status(200).send({ stakeholderId, type, role });
+    } catch (error) {
+        console.error(`error: ${error}`);
+        res.status(500).send(`${error}`);
+    }
+});
+
+stakeholder.get("/total-number", async (req, res) => {
+    try {
+        const { contract } = req;
+        const totalStakeholders = await getTotalNumberOfStakeholders(contract);
+        res.status(200).send(totalStakeholders);
+    } catch (error) {
+        console.error(`error: ${error}`);
+        res.status(500).send(`${error}`);
+    }
+});
+
+/// @dev: stakeholder is always added to the DB and created onchain in the same function.
+// Order to be determined.
 stakeholder.post("/create", async (req, res) => {
     const { contract } = req;
     const { name, issuer_assigned_id, stakeholder_type, current_relationship, primary_contact, contact_info, comments } = req.body;
@@ -34,7 +60,31 @@ stakeholder.post("/create", async (req, res) => {
         res.status(200).send({ stakeholder });
     } catch (error) {
         console.error(`error: ${error}`);
-        res.status(500).send({ error });
+        res.status(500).send(`${error}`);
+    }
+});
+
+stakeholder.post("/add-wallet", async (req, res) => {
+    const { contract } = req;
+    const { id, wallet } = req.body;
+    try {
+        await addWalletToStakeholder(contract, id, wallet);
+        res.status(200).send("Success");
+    } catch (error) {
+        console.error(`error: ${error}`);
+        res.status(500).send(`${error}`);
+    }
+});
+
+stakeholder.post("/remove-wallet", async (req, res) => {
+    const { contract } = req;
+    const { id, wallet } = req.body;
+    try {
+        await removeWalletFromStakeholder(contract, id, wallet);
+        res.status(200).send("Success");
+    } catch (error) {
+        console.error(`error: ${error}`);
+        res.status(500).send(`${error}`);
     }
 });
 
