d3850b22d63218073a5986942825bb7e3a0a1438
Victor Mimo <victormimoc@gmail.com>
2023-08-23 13:52:15 -0300

    updating stock issuance with new validation

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
+import stockIssuanceSchema from "../../ocf/schema/objects/transactions/issuance/StockIssuance.schema.json" assert { type: "json" };
 import { convertAndCreateIssuanceStockOnchain } from "../db/controllers/transactions/issuanceController.js";
 import { convertAndCreateTransferStockOnchain } from "../db/controllers/transactions/transferController.js";
+import validateInputAgainstOCF from "../utils/validateInputAgainstSchema.js";
 
 const transactions = Router();
 
@@ -8,12 +11,22 @@ transactions.post("/issuance/stock", async (req, res) => {
     const { contract } = req;
 
     try {
-        await convertAndCreateIssuanceStockOnchain(contract, req.body);
+        const incomingStockIssuance = {
+            id: uuid(),
+            security_id: uuid(),
+            date: new Date().toISOString().slice(0, 10),
+            object_type: "TX_STOCK_ISSUANCE",
+            ...req.body,
+        };
 
-        res.status(200).send("success");
+        await validateInputAgainstOCF(incomingStockIssuance, stockIssuanceSchema);
+
+        await convertAndCreateIssuanceStockOnchain(contract, incomingStockIssuance);
+
+        res.status(200).send({ stockIssuance: incomingStockIssuance });
     } catch (error) {
         console.error(`error: ${error}`);
-        res.status(500).send({ error });
+        res.status(500).send(`${error}`);
     }
 });
 
@@ -25,9 +38,9 @@ transactions.post("/transfer/stock", async (req, res) => {
         await convertAndCreateTransferStockOnchain(contract, req.body);
 
         res.status(200).send("success");
-    } catch (err) {
+    } catch (error) {
         console.error(`error: ${error}`);
-        res.status(500).send({ error });
+        res.status(500).send(`${error}`);
     }
 });
 
