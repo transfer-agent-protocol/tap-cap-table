64b8bd9b0eb515f451f0e55ba6970aeec9b1ca74
Victor Mimo <victormimoc@gmail.com>
2023-08-23 12:45:01 -0300

    simplifying controller

--- src/db/controllers/stockClassController.js
+++ src/db/controllers/stockClassController.js
@@ -1,28 +1,14 @@
-import { convertUUIDToBytes16 } from "../../utils/convertUUID.js";
 import { toScaledBigNumber } from "../../utils/convertToFixedPointDecimals.js";
-
-import validateInput from "../../utils/validateInputAgainstSchema.js";
-import stockClassSchema from "../../../ocf/schema/objects/StockClass.schema.json" assert { type: "json" };
-
-export const validateStockClass = async (data) => {
-    // First: validate the manifest against OCF, for the stock class schema
-    const { isValid, errors } = await validateInput(data, stockClassSchema);
-
-    if (isValid) {
-        console.log("Schema is valid ", isValid);
-    } else {
-        throw new Error(JSON.stringify(errors, null, 2));
-    }
-};
+import { convertUUIDToBytes16 } from "../../utils/convertUUID.js";
 
 /// @dev: controller handles conversion from OCF type to Onchain types and creates the stock class.
 export const convertAndReflectStockClassOnchain = async (contract, stockClass) => {
     // First: convert OCF Types to Onchain Types
-    const stockClassIdBytes16 = convertUUIDToBytes16(stockClass._id);
+    const stockClassIdBytes16 = convertUUIDToBytes16(stockClass.id);
     const scaledSharePrice = toScaledBigNumber(stockClass.price_per_share.amount);
     const scaledShares = toScaledBigNumber(stockClass.initial_shares_authorized);
 
-    console.log("Stock Class ID offchain", stockClass._id);
+    console.log("Stock Class ID offchain", stockClass.id);
     console.log("Stock Class ID converted to bytes16", stockClassIdBytes16);
 
     // Second: create stock class onchain
