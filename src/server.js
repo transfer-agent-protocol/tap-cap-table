e1268a3d044a2fd10ee42d8f492ec8cf70f1df24
Victor Mimo <victormimoc@gmail.com>
2023-09-15 22:39:08 -0300

    updating contract middleware to set up listener per contract

--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
-import express, { json, urlencoded } from "express";
 import { config } from "dotenv";
+import express, { json, urlencoded } from "express";
 config();
 
 import connectDB from "./db/config/mongoose.js";
 
-import startOnchainListeners from "./chain-operations/transactionListener.js";
 import getContractInstance from "./chain-operations/getContractInstances.js";
+import startOnchainListeners from "./chain-operations/transactionListener.js";
 
 // Routes
 import mainRoutes from "./routes/index.js";
 import issuerRoutes from "./routes/issuer.js";
 import stakeholderRoutes from "./routes/stakeholder.js";
 import stockClassRoutes from "./routes/stockClass.js";
-import transactionRoutes from "./routes/transactions.js";
 import stockLegendRoutes from "./routes/stockLegend.js";
 import stockPlanRoutes from "./routes/stockPlan.js";
+import transactionRoutes from "./routes/transactions.js";
 import valuationRoutes from "./routes/valuation.js";
 import vestingTermsRoutes from "./routes/vestingTerms.js";
+
+import { readIssuerById } from "./db/operations/read.js";
 
 const app = express();
 
@@ -24,7 +26,7 @@ const app = express();
 connectDB();
 
 const PORT = 8080;
-const CHAIN = "local"; // TODO change this to prod or env style variable
+const CHAIN = process.env.CHAIN;
 
 // Middlewares
 const chainMiddleware = (req, res, next) => {
@@ -32,19 +34,40 @@ const chainMiddleware = (req, res, next) => {
     next();
 };
 
+// Centralized contract manager/cache
+const contractCache = {};
+
+console.log("contract cache", contractCache);
+
+// Middleware to get or create contract instance
 const contractMiddleware = async (req, res, next) => {
-    const { contract, provider } = await getContractInstance(CHAIN);
-    req.contract = contract;
-    req.provider = provider;
+    if (!req.body.issuerId) {
+        console.log("no issuer ID");
+        res.status(400).send("issuerId required");
+    }
+
+    console.log("issuer id in middleware ", req.body.issuerId);
+    const issuer = await readIssuerById(req.body.issuerId);
+
+    // Check if contract instance already exists in cache
+    if (!contractCache[req.body.issuerId]) {
+        const { contract, provider } = await getContractInstance(CHAIN, issuer.deployed_to);
+        contractCache[req.body.issuerId] = { contract, provider };
+
+        // Initialize listener for this contract
+        startOnchainListeners(contract, provider);
+    }
+
+    req.contract = contractCache[req.body.issuerId].contract;
+    req.provider = contractCache[req.body.issuerId].provider;
     next();
 };
-
 app.use(urlencoded({ limit: "50mb", extended: true }));
 app.use(json({ limit: "50mb" }));
 app.enable("trust proxy");
 
 app.use("/", chainMiddleware, mainRoutes);
-app.use("/issuer", chainMiddleware, contractMiddleware, issuerRoutes);
+app.use("/issuer", chainMiddleware, issuerRoutes);
 app.use("/stakeholder", contractMiddleware, stakeholderRoutes);
 app.use("/stock-class", contractMiddleware, stockClassRoutes);
 // No middleware required since these are only created offchain
@@ -58,5 +81,4 @@ app.use("/transactions/", contractMiddleware, transactionRoutes);
 
 app.listen(PORT, async () => {
     console.log(`🚀  Server successfully launched. Access at: http://localhost:${PORT}`);
-    await startOnchainListeners(CHAIN);
 });
