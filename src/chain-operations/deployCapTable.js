e32bdf21d17d1ce7d87d08d56b214e004ddcdf23
Victor Mimo <victormimoc@gmail.com>
2023-09-17 13:16:40 -0300

    adding scaffold to verify programmatically, not used right now

--- src/chain-operations/deployCapTable.js
+++ src/chain-operations/deployCapTable.js
@@ -1,7 +1,8 @@
-import { ethers } from "ethers";
 import { config } from "dotenv";
+import { ethers } from "ethers";
 config();
 
+import axios from "axios";
 import CAP_TABLE from "../../chain/out/CapTable.sol/CapTable.json" assert { type: "json" };
 const { abi, bytecode } = CAP_TABLE;
 
@@ -29,6 +30,7 @@ async function deployCapTableOptimismGoerli(issuerId, issuerName) {
     const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_POET_TEST;
 
     const provider = new ethers.providers.JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_URL);
+    const providerEtherscan = new ethers.providers.EtherscanProvider("optimism-goerli", process.env.ETHERSCAN_OPTIMISM_API_KEY);
     const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
     const factory = new ethers.ContractFactory(abi, bytecode, wallet);
     const contract = await factory.deploy(issuerId, issuerName);
@@ -38,6 +40,39 @@ async function deployCapTableOptimismGoerli(issuerId, issuerName) {
     console.log("Contract mined!");
 
     return contract.address;
+}
+
+const chainUrlMapper = (chain) => {
+    if (chain === "optimism-goerli") return "https://api.goerli-optimism.etherscan.io/api";
+};
+
+// Scaffold code to verify contracts programmatically, WIP and not used at the moment.
+async function verifyContractOnEtherscan(chain, address) {
+    const apiKey = process.env.ETHERSCAN_OPTIMISM_API_KEY;
+    const contractAddress = address;
+    const sourceCode = fs.readFileSync("path_to_flattened_contract.sol", "utf8");
+    const contractName = "CapTable";
+    const compilerVersion = "v0.8.20";
+    const optimizationUsed = 1; // 0 for no, 1 for yes
+    const runs = 200; // This is the default
+    const constructorArguments = ""; // If your contract has constructor arguments, they need to be ABI-encoded and provided here.
+
+    const apiUrl = chainUrlMapper(chain);
+
+    const response = await axios.post(apiUrl, {
+        apikey: apiKey,
+        module: "contract",
+        action: "verifysourcecode",
+        contractaddress: contractAddress,
+        sourceCode: sourceCode,
+        contractname: contractName,
+        compilerversion: compilerVersion,
+        optimizationUsed: optimizationUsed,
+        runs: runs,
+        constructorArguements: constructorArguments,
+    });
+
+    console.log("response", response);
 }
 
 async function deployCapTable(chain, issuerId, issuerName) {
