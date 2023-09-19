ebbbd5c20f986d30055b153e6acd81bb79616cd1
Victor Mimo <victormimoc@gmail.com>
2023-09-19 05:49:25 -0300

    updating fetching contract instance with new provider method

--- src/chain-operations/getContractInstances.js
+++ src/chain-operations/getContractInstances.js
@@ -5,10 +5,11 @@ config();
 import CAP_TABLE from "../../chain/out/CapTable.sol/CapTable.json" assert { type: "json" };
 const { abi } = CAP_TABLE;
 
+//TODO: reflect ethers v6 changes here.
 async function getLocalContractInstance(address) {
     // if deployed using forge script
     //const CONTRACT_ADDRESS_LOCAL = require("../chain/broadcast/CapTable.s.sol/31337/run-latest.json").transactions[0].contractAddress;
-    const CONTRACT_ADDRESS_LOCAL = address; // fill in from capTableFactory
+    const CONTRACT_ADDRESS_LOCAL = address;
 
     const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_FAKE_ACCOUNT;
     const LOCAL_RPC_URL = process.env.LOCAL_RPC_URL;
@@ -18,7 +19,7 @@ async function getLocalContractInstance(address) {
         name: "local",
     };
 
-    const provider = new ethers.providers.JsonRpcProvider(LOCAL_RPC_URL, customNetwork);
+    const provider = new ethers.JsonRpcProvider(LOCAL_RPC_URL, customNetwork);
     const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
     const contract = new ethers.Contract(CONTRACT_ADDRESS_LOCAL, abi, wallet);
 
@@ -31,7 +32,7 @@ async function getOptimismGoerliContractInstance(address) {
     const CONTRACT_ADDRESS_OPTIMISM_GOERLI = address;
     const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_POET_TEST;
 
-    const provider = new ethers.providers.JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_URL);
+    const provider = new ethers.JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_URL);
     const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
     const contract = new ethers.Contract(CONTRACT_ADDRESS_OPTIMISM_GOERLI, abi, wallet);
 
