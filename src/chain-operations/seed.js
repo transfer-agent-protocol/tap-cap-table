f0d895941b1dc8905737c1f925b72ce4d79cc6dd
Victor Mimo <victormimoc@gmail.com>
2023-09-20 11:00:25 -0300

    adding function to seed active positions and active security ids

--- src/chain-operations/seed.js
+++ src/chain-operations/seed.js
@@ -2,6 +2,8 @@ import { convertAndReflectStakeholderOnchain } from "../controllers/stakeholderC
 import { convertAndReflectStockClassOnchain } from "../controllers/stockClassController.js";
 import { convertAndSeedIssuanceStockOnchain, convertAndSeedTransferStockOnchain } from "../controllers/transactions/seed.js";
 import { getAllIssuerDataById } from "../db/operations/read.js";
+import { convertUUIDToBytes16 } from "../utils/convertUUID.js";
+import { toScaledBigNumber, convertTimeStampToUint40 } from "../utils/convertToFixedPointDecimals.js";
 
 export const initiateSeeding = async (uuid, contract) => {
     console.log("Intiating Seeding...");
@@ -31,3 +33,36 @@ export const initiateSeeding = async (uuid, contract) => {
         await convertAndSeedTransferStockOnchain(contract, stockTransfer);
     }
 };
+
+export const seedActivePositionsAndActiveSecurityIds = async (arrays, contract) => {
+    const { stakeholders, stockClasses, quantities, securityIds, sharePrices, timestamps } = arrays;
+
+    console.log(" stakeholders ", stakeholders);
+    console.log(" stockClasses ", stockClasses);
+    console.log(" quantities ", quantities);
+    console.log(" securityIds ", securityIds);
+    console.log(" sharePrices ", sharePrices);
+    console.log(" timestamps ", timestamps);
+
+    // convert them.
+    const stakeholderIdsBytes16 = stakeholders.map((stakeholder) => convertUUIDToBytes16(stakeholder));
+    const stockClassIdsBytes16 = stockClasses.map((stockClass) => convertUUIDToBytes16(stockClass));
+    const quantitiesScaled = quantities.map((quantity) => toScaledBigNumber(quantity));
+    const securityIdsBytes16 = securityIds.map((securityId) => convertUUIDToBytes16(securityId));
+    const sharePricesScaled = sharePrices.map((sharePrice) => toScaledBigNumber(sharePrice));
+    const timestampsScaled = timestamps.map((timestamp) => convertTimeStampToUint40(timestamp));
+
+    const tx = await contract.seedMultipleActivePositionsAndSecurityIds(
+        stakeholderIdsBytes16,
+        securityIdsBytes16,
+        stockClassIdsBytes16,
+        quantitiesScaled,
+        sharePricesScaled,
+        timestampsScaled
+    );
+
+    await tx.wait();
+
+    console.log("Seeded Active Positions and Active Security Ids onchain");
+};
+//
