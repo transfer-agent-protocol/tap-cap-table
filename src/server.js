35aba699c2582d578721ece0ef95aa4ad857b3ad
Adam Momen <adammuman81@gmail.com>
2023-10-10 18:33:54 +0300

    Add: simple authentication system based on hardcoded api key

--- src/server.js
+++ src/server.js
@@ -30,9 +30,23 @@ connectDB();
 const PORT = 8080;
 const CHAIN = process.env.CHAIN;
 
+const API_KEY = process.env.API_KEY
+
+
 // Middlewares
 const chainMiddleware = (req, res, next) => {
     req.chain = CHAIN;
+    next();
+};
+
+const authenticationMiddleware = (req, res, next) => {
+    const apiKey = req.headers['x-api-key'];
+    if (!apiKey) {
+        return res.status(401).send('API key missing');
+    }
+    if (apiKey !== API_KEY) {
+        return res.status(403).send('Invalid API key');
+    }
     next();
 };
 
@@ -64,19 +78,19 @@ app.use(urlencoded({ limit: "50mb", extended: true }));
 app.use(json({ limit: "50mb" }));
 app.enable("trust proxy");
 
-app.use("/", chainMiddleware, mainRoutes);
-app.use("/issuer", chainMiddleware, issuerRoutes);
-app.use("/stakeholder", contractMiddleware, stakeholderRoutes);
-app.use("/stock-class", contractMiddleware, stockClassRoutes);
+app.use("/", authenticationMiddleware, chainMiddleware, mainRoutes);
+app.use("/issuer", authenticationMiddleware, chainMiddleware, issuerRoutes);
+app.use("/stakeholder", authenticationMiddleware, contractMiddleware, stakeholderRoutes);
+app.use("/stock-class", authenticationMiddleware, contractMiddleware, stockClassRoutes);
 // No middleware required since these are only created offchain
-app.use("/stock-legend", stockLegendRoutes);
-app.use("/stock-plan", stockPlanRoutes);
-app.use("/valuation", valuationRoutes);
-app.use("/vesting-terms", vestingTermsRoutes);
-app.use("/historical-transactions", historicalTransactions);
+app.use("/stock-legend", authenticationMiddleware, stockLegendRoutes);
+app.use("/stock-plan", authenticationMiddleware, stockPlanRoutes);
+app.use("/valuation", authenticationMiddleware, valuationRoutes);
+app.use("/vesting-terms", authenticationMiddleware, vestingTermsRoutes);
+app.use("/historical-transactions", authenticationMiddleware, historicalTransactions);
 
 // transactions
-app.use("/transactions/", contractMiddleware, transactionRoutes);
+app.use("/transactions/", authenticationMiddleware, contractMiddleware, transactionRoutes);
 
 app.listen(PORT, async () => {
     console.log(`🚀  Server successfully launched. Access at: http://localhost:${PORT}`);
