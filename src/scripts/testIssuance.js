54fca16ee63ee6c66494d9987158618c8b42038a
Victor Mimo <victormimoc@gmail.com>
2023-11-03 16:43:37 -0400

    simplifying stock issuance script

--- src/scripts/testIssuance.js
+++ src/scripts/testIssuance.js
@@ -9,17 +9,21 @@ connectDB();
 const main = async () => {
     console.log("⏳ | Creating stock issuance");
 
-    const lastIssuer = await Issuer.find().sort({ _id: -1 }).limit(1);
-    const { _id: issuerId } = lastIssuer[0];
+    // const lastIssuer = await Issuer.find().sort({ _id: -1 }).limit(1);
+    // const { _id: issuerId } = lastIssuer[0];
 
-    const lastStakeholder = await Stakeholder.find().sort({ _id: -1 }).limit(1);
-    const { _id: stakeholderId } = lastStakeholder[0];
+    // const lastStakeholder = await Stakeholder.find().sort({ _id: -1 }).limit(1);
+    // const { _id: stakeholderId } = lastStakeholder[0];
 
-    const lastStockClass = await StockClass.find().sort({ _id: -1 }).limit(1);
-    const { _id: stockClassId } = lastStockClass[0];
-    console.log({ issuerId, stakeholderId, stockClassId });
+    // const lastStockClass = await StockClass.find().sort({ _id: -1 }).limit(1);
+    // const { _id: stockClassId } = lastStockClass[0];
 
-    // create stockIssuance
+    // console.log({ issuerId, stakeholderId, stockClassId });
+
+    const issuerId = "9429a8e6-cd9b-4516-bff7-9473c96dbe04";
+    const stakeholderId = "89c76e6e-39a8-47e1-b72e-e73a404120db";
+    const stockClassId = "e3363214-be74-4084-bbf1-029b9cb9973f";
+
     const stockIssuanceResponse = await axios.post(
         "http://localhost:8080/transactions/issuance/stock",
         stockIssuance(issuerId, stakeholderId, stockClassId, "2000", "1.2")
