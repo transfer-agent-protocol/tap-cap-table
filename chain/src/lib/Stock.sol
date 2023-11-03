5a0b504e4dfbd354a468f99eb7ab227db89ee018
Victor Mimo <victormimoc@gmail.com>
2023-11-03 16:43:04 -0400

    template for Stock Lib

---
+++ chain/src/lib/Stock.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { StockAcceptance, ActivePositions, ActivePosition, Issuer, StockClass, SecIdsStockClass } from "./Structs.sol";
+import "./TxHelper.sol";
+import "./DeleteContext.sol";
+
+library StockLib {}
