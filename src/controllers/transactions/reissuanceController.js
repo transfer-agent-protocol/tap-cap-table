7d4a805680f8efbbe268f2f96883622093a28f66
Adam Momen <adammuman81@gmail.com>
2023-11-08 03:37:28 +0800

    Modify issuance controller

--- src/controllers/transactions/reissuanceController.js
+++ src/controllers/transactions/reissuanceController.js
@@ -9,11 +9,13 @@ export const convertAndCreateReissuanceStockOnchain = async (
         reason_text,
         comments = [] }
 ) => {
-    const secIdBytes16 = convertUUIDToBytes16(security_id);
-    const stakeHolderIdBytes16 = convertUUIDToBytes16(stakeholderId);
-    const stockClassIdBytes16 = convertUUIDToBytes16(stockClassId);
-    const resultingSecIds  = resulting_security_ids.map( sId => convertUUIDToBytes16(sId))
-
-    const tx = await contract.reissueStock(stakeHolderIdBytes16, stockClassIdBytes16, resultingSecIds, secIdBytes16, comments, reason_text);
+    const resulting_security_ids_b16 = resulting_security_ids.map(sId => convertUUIDToBytes16(sId)),
+    const tx = await contract.reissueStock({
+        stakeholder_id: convertUUIDToBytes16(stakeholderId),
+        stock_class_id: convertUUIDToBytes16(stockClassId),
+        security_id: convertUUIDToBytes16(security_id),
+        comments,
+        reason_text
+    }, resulting_security_ids_b16);
     await tx.wait();
 };
