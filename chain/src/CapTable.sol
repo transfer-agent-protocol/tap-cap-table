005c2bd260fe68ef5c15f36e8c2356ff688e077f
Adam Momen <adammuman81@gmail.com>
2023-11-24 09:58:04 +0800

    remove console.log import

--- chain/src/CapTable.sol
+++ chain/src/CapTable.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "forge-std/console.sol";
 
 import { AccessControlDefaultAdminRules } from "openzeppelin-contracts/contracts/access/AccessControlDefaultAdminRules.sol";
 import { ICapTable } from "./ICapTable.sol";
