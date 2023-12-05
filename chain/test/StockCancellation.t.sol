f931ddd8fcbefc72dbe3e5b011d60e575487e532
Victor Mimo <victormimoc@gmail.com>
2023-12-05 14:14:42 -0500

    refactoring issueStock helper code away

--- chain/test/StockCancellation.t.sol
+++ chain/test/StockCancellation.t.sol
@@ -4,57 +4,13 @@ pragma solidity ^0.8.20;
 import "forge-std/console.sol";
 
 import "./CapTable.t.sol";
-import {
-    InitialShares,
-    IssuerInitialShares,
-    StockClassInitialShares,
-    Issuer,
-    StockClass,
-    StockIssuanceParams,
-    ShareNumbersIssued,
-    StockIssuance,
-    StockTransfer,
-    StockParams
-} from "../src/lib/Structs.sol";
+import { InitialShares, IssuerInitialShares, StockClassInitialShares, Issuer, StockClass, StockIssuanceParams, ShareNumbersIssued, StockIssuance, StockTransfer, StockParams } from "../src/lib/Structs.sol";
 
 contract StockCancellationTest is CapTableTest {
-    function _createStockClassAndStakeholder(uint256 stockClassInitialSharesAuthorized)
-        private
-        returns (bytes16, bytes16)
-    {
-        bytes16 stakeholderId = 0xd3373e0a4dd940000000000000000005;
-        capTable.createStakeholder(stakeholderId, "INDIVIDUAL", "EMOLOYEE");
-
-        bytes16 stockClassId = 0xd3373e0a4dd940000000000000000000;
-        capTable.createStockClass(stockClassId, "Common", 100, stockClassInitialSharesAuthorized);
-
-        return (stockClassId, stakeholderId);
-    }
-
     function testFullStockCancellation() public {
-        // Create stock class and stakeholder
-        (bytes16 stockClassId, bytes16 stakeholderId) = _createStockClassAndStakeholder(1000000);
+        (bytes16 stockClassId, bytes16 stakeholderId) = createStockClassAndStakeholder(1000000);
 
-        // Issue stock
-        StockIssuanceParams memory issuanceParams = StockIssuanceParams({
-            stock_class_id: stockClassId,
-            stock_plan_id: 0x00000000000000000000000000000000,
-            share_numbers_issued: ShareNumbersIssued(0, 0),
-            share_price: 100,
-            quantity: 1000,
-            vesting_terms_id: 0x00000000000000000000000000000000,
-            cost_basis: 50,
-            stock_legend_ids: new bytes16[](0),
-            issuance_type: "RSA",
-            comments: new string[](0),
-            custom_id: "R2-D2",
-            stakeholder_id: stakeholderId,
-            board_approval_date: "2023-01-01",
-            stockholder_approval_date: "2023-01-02",
-            consideration_text: "For services rendered",
-            security_law_exemptions: new string[](0)
-        });
-        capTable.issueStock(issuanceParams);
+        issueStock(stockClassId, stakeholderId);
 
         // Cancel stock
         uint256 lastTransactionIndex = capTable.getTransactionsCount() - 1;
@@ -65,7 +21,7 @@ contract StockCancellationTest is CapTableTest {
                 stakeholder_id: stakeholderId,
                 stock_class_id: stockClassId,
                 security_id: lastIssuance.security_id,
-                reason_text: "I don't like this stock",
+                reason_text: "The issued party failed to meet their obligations",
                 comments: new string[](0)
             }),
             lastIssuance.params.quantity
@@ -76,32 +32,14 @@ contract StockCancellationTest is CapTableTest {
         lastTransaction = capTable.transactions(lastTransactionIndex);
         StockCancellation memory lastCancellation = abi.decode(lastTransaction, (StockCancellation));
         assertEq(lastCancellation.object_type, "TX_STOCK_CANCELLATION");
+        assertEq(lastCancellation.quantity, lastIssuance.params.quantity);
     }
 
     function testPartialStockCancellation() public {
         // Create stock class and stakeholder
-        (bytes16 stockClassId, bytes16 stakeholderId) = _createStockClassAndStakeholder(1000000);
+        (bytes16 stockClassId, bytes16 stakeholderId) = createStockClassAndStakeholder(1000000);
 
-        // Issue stock
-        StockIssuanceParams memory issuanceParams = StockIssuanceParams({
-            stock_class_id: stockClassId,
-            stock_plan_id: 0x00000000000000000000000000000000,
-            share_numbers_issued: ShareNumbersIssued(0, 0),
-            share_price: 100,
-            quantity: 1000,
-            vesting_terms_id: 0x00000000000000000000000000000000,
-            cost_basis: 50,
-            stock_legend_ids: new bytes16[](0),
-            issuance_type: "RSA",
-            comments: new string[](0),
-            custom_id: "R2-D2",
-            stakeholder_id: stakeholderId,
-            board_approval_date: "2023-01-01",
-            stockholder_approval_date: "2023-01-02",
-            consideration_text: "For services rendered",
-            security_law_exemptions: new string[](0)
-        });
-        capTable.issueStock(issuanceParams);
+        issueStock(stockClassId, stakeholderId);
 
         // Cancel part of the stock
         uint256 lastTransactionIndex = capTable.getTransactionsCount() - 1;
