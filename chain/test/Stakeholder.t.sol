cb2ee6d90df6bd3ddcd67f1e0341489efa4b38e9
Victor Mimo <victormimoc@gmail.com>
2023-12-06 12:09:36 -0500

    fixing test name

--- chain/test/Stakeholder.t.sol
+++ chain/test/Stakeholder.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "./CapTable.t.sol";
 
-contract StakeholderTests is CapTableTest {
+contract StakeholderTest is CapTableTest {
     function testCreateStakeholder() public {
         bytes16 stakeholderId = 0xf47ac10b58cc4372a5670e02b2c3d479;
         capTable.createStakeholder(stakeholderId, "INDIVIDUAL", "ADVISOR");
