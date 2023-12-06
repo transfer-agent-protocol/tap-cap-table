130553d260bd9f047973ecae3a7d7e5491851d2e
Victor Mimo <victormimoc@gmail.com>
2023-12-06 12:09:00 -0500

    fixing test name

--- chain/test/Issuer.t.sol
+++ chain/test/Issuer.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "./CapTable.t.sol";
 
-contract IssuerTests is CapTableTest {
+contract IssuerTest is CapTableTest {
     function testIssuerInitialization() public {
         (bytes16 id, string memory legal_name, uint256 shares_issued, uint256 shares_authorized) = capTable.issuer();
         bytes16 expectedId = 0xd3373e0a4dd9430f8a563281f2800e1e;
