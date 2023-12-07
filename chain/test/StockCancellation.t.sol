084da1f8b60c64782091719f8c051e5617ab4568
Victor Mimo <victormimoc@gmail.com>
2023-12-07 14:06:55 -0500

    updating stock cancellation with new activePosition error and shares_issued for issuer and stock class

--- chain/test/StockCancellation.t.sol
+++ chain/test/StockCancellation.t.sol
@@ -12,10 +12,11 @@ contract StockCancellationTest is CapTableTest {
 
         issueStock(stockClassId, stakeholderId, 1000);
 
-        // Cancel stock
         uint256 lastTransactionIndex = capTable.getTransactionsCount() - 1;
         bytes memory lastTransaction = capTable.transactions(lastTransactionIndex);
         StockIssuance memory lastIssuance = abi.decode(lastTransaction, (StockIssuance));
+
+        // Cancel stock
         capTable.cancelStock(
             StockParams({
                 stakeholder_id: stakeholderId,
@@ -41,23 +42,20 @@ contract StockCancellationTest is CapTableTest {
 
         issueStock(stockClassId, stakeholderId, 1000);
 
-        uint256 partialCancellationQuantity = 500; // Cancel only part of the stock
-        uint256 quantityAvailable = 0;
+        bytes16 nonExistentSecId = bytes16("0xd3373e0a4dd86");
 
-        // if quantity is 0, there's no ActivePosition that exists.
-        bytes memory expectedError = abi.encodeWithSignature(
-            "InsufficientSharesOrNoActivePosition(uint256,uint256)",
-            quantityAvailable,
-            partialCancellationQuantity
-        );
+        // Expecting the ActivePositionNotFound revert
+        bytes memory expectedError = abi.encodeWithSignature("ActivePositionNotFound(bytes16,bytes16)", stakeholderId, nonExistentSecId);
         vm.expectRevert(expectedError);
+
+        uint256 partialCancellationQuantity = 500;
 
         // cancel a stock with a security_id that doesn't exist, therefore no ActivePosition present.
         capTable.cancelStock(
             StockParams({
                 stakeholder_id: stakeholderId,
                 stock_class_id: stockClassId,
-                security_id: bytes16("0xd3373e0a4dd86"),
+                security_id: nonExistentSecId,
                 reason_text: "Partial cancellation",
                 comments: new string[](0)
             }),
@@ -78,7 +76,7 @@ contract StockCancellationTest is CapTableTest {
         uint256 quantityToCancel = 1200; // Cancel 200 more than available
 
         // if quantity is 0, there's no ActivePosition that exists.
-        bytes memory expectedError = abi.encodeWithSignature("InsufficientSharesOrNoActivePosition(uint256,uint256)", 1000, quantityToCancel);
+        bytes memory expectedError = abi.encodeWithSignature("InsufficientShares(uint256,uint256)", 1000, quantityToCancel);
         vm.expectRevert(expectedError);
 
         capTable.cancelStock(
@@ -100,15 +98,15 @@ contract StockCancellationTest is CapTableTest {
         issueStock(stockClassId, stakeholderId, 1000);
 
         // Cancel part of the stock
-        uint256 lastTransactionIndex = capTable.getTransactionsCount() - 1;
-        bytes memory lastTransaction = capTable.transactions(lastTransactionIndex);
-        StockIssuance memory lastIssuance = abi.decode(lastTransaction, (StockIssuance));
+        uint256 issuanceTx = capTable.getTransactionsCount() - 1;
+        bytes memory issuance = capTable.transactions(issuanceTx);
+        StockIssuance memory firstIssuance = abi.decode(issuance, (StockIssuance));
         uint256 partialCancellationQuantity = 500; // Cancel only part of the stock
         capTable.cancelStock(
             StockParams({
                 stakeholder_id: stakeholderId,
                 stock_class_id: stockClassId,
-                security_id: lastIssuance.security_id,
+                security_id: firstIssuance.security_id,
                 reason_text: "Partial cancellation",
                 comments: new string[](0)
             }),
@@ -118,15 +116,22 @@ contract StockCancellationTest is CapTableTest {
         // Assert last transaction is of type issuance with the remaining amount
         uint256 secondLastTransactionIndex = capTable.getTransactionsCount() - 2;
         bytes memory secondLastTransaction = capTable.transactions(secondLastTransactionIndex);
-        StockIssuance memory secondLastIssuance = abi.decode(secondLastTransaction, (StockIssuance));
-        assertEq(secondLastIssuance.object_type, "TX_STOCK_ISSUANCE");
-        assertEq(secondLastIssuance.params.quantity, lastIssuance.params.quantity - partialCancellationQuantity);
+        StockIssuance memory secondIssuance = abi.decode(secondLastTransaction, (StockIssuance));
+        assertEq(secondIssuance.object_type, "TX_STOCK_ISSUANCE");
+        assertEq(secondIssuance.params.quantity, firstIssuance.params.quantity - partialCancellationQuantity);
 
         // Assert last transaction is of type cancellation
-        lastTransactionIndex = capTable.getTransactionsCount() - 1;
-        lastTransaction = capTable.transactions(lastTransactionIndex);
-        StockCancellation memory lastCancellation = abi.decode(lastTransaction, (StockCancellation));
-        assertEq(lastCancellation.object_type, "TX_STOCK_CANCELLATION");
-        assertEq(lastCancellation.quantity, partialCancellationQuantity);
+        uint256 lastTransactionIndex = capTable.getTransactionsCount() - 1;
+        bytes memory lastTransaction = capTable.transactions(lastTransactionIndex);
+        StockCancellation memory cancellation = abi.decode(lastTransaction, (StockCancellation));
+        assertEq(cancellation.object_type, "TX_STOCK_CANCELLATION");
+        assertEq(cancellation.quantity, partialCancellationQuantity);
+
+        // Assert issuer and stock class shares_issued
+        (, , uint256 issuerSharesIssued, ) = capTable.issuer();
+        assertEq(issuerSharesIssued, firstIssuance.params.quantity - partialCancellationQuantity);
+
+        (, , , , uint256 stockClassSharesIssued) = capTable.getStockClassById(stockClassId);
+        assertEq(stockClassSharesIssued, firstIssuance.params.quantity - partialCancellationQuantity);
     }
 }
