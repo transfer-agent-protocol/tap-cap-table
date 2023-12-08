3cd07fac2caa680ae4a150851b2ee40208bd2735
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-12-08 16:25:38 -0500

    Use .env RPC url, improve deployLib with better logging and structure

--- src/scripts/deployAndLinkLibs.js
+++ src/scripts/deployAndLinkLibs.js
@@ -7,6 +7,8 @@ import sleep from "../utils/sleep.js";
 config();
 
 const privateKey = process.env.PRIVATE_KEY_FAKE_ACCOUNT;
+// Use environment variable for the RPC URL
+const LOCAL_RPC_URL = process.env.LOCAL_RPC_URL;
 
 const rootDirectory = "./src/lib";
 const excludeDirectory = "./src/lib/transactions";
@@ -70,37 +72,65 @@ function getAllLibraries(dirPath) {
 
 async function deployLib(lib, libs) {
     return new Promise((resolve, reject) => {
-        console.log(`Deploying ${lib.libraryName || lib.fileName}\n`);
-        const librariesDepsArgs = lib.deps.map((idx) => ["--libraries", `${libs[idx].path}:${libs[idx].libraryName}:${libs[idx].address}`]).flat();
+        console.log(`Starting deployment for ${lib.libraryName || lib.fileName}`);
+
+        // Logging the dependencies and their addresses
+        const librariesDepsArgs = lib.deps
+            .map((idx) => {
+                console.log(`Dependency: ${libs[idx].libraryName}, Address: ${libs[idx].address}`);
+                return ["--libraries", `${libs[idx].path}:${libs[idx].libraryName}:${libs[idx].address}`];
+            })
+            .flat();
         console.log("librariesDepsArgs:", librariesDepsArgs);
+
+        // Forge command arguments
         const args = [
             "c",
             "-r",
-            "http://127.0.0.1:8545",
+            // Using the LOCAL_RPC_URL from .env
+            LOCAL_RPC_URL,
             "--chain",
-            31337,
+            // TODO: Handle changing Anvil's chain id better
+            // 31337,
+            // This is Arbitrum Orbit's chain id
+            32586980208,
             "--private-key",
             privateKey,
             `${lib.path}:${lib.libraryName || lib.fileName}`,
-            // "--via-ir",
             "--json",
             ...librariesDepsArgs,
         ];
+        // TODO: only log when things break console.log(`Forge command arguments: ${args.join(" ")}`);
+
+        // Executing the forge command
         const subprocess = spawn("forge", args);
 
         subprocess.stdout.on("data", (data) => {
-            lib.address = JSON.parse(data).deployedTo;
-            console.log(`${lib.fileName} Deployed Successfully - Address: ${lib.address}\n`);
+            console.log(`stdout: ${data}`);
+            try {
+                lib.address = JSON.parse(data).deployedTo;
+                console.log(`${lib.fileName} Deployed Successfully - Address: ${lib.address}`);
+            } catch (err) {
+                console.error(`Error parsing JSON from stdout: ${err}`);
+            }
         });
 
         subprocess.stderr.on("data", (data) => {
-            reject(new Error(`${lib.fileName}::stderr: ${data}`));
+            const errorDetails = data.toString(); // Convert Buffer to string
+            console.error(`stderr: ${errorDetails}`);
+        });
+
+        subprocess.on("error", (error) => {
+            console.error(`Spawn error: ${error}`);
+            reject(new Error(`Error executing forge command for ${lib.fileName}`));
         });
 
         subprocess.on("close", (code) => {
             if (code !== 0) {
-                reject(new Error(`${lib.fileName}::child process exited with code ${code}`));
+                console.error(`${lib.fileName} deployment failed with exit code ${code}`);
+                reject(new Error(`${lib.fileName} deployment failed. Details in the logs above.`));
             } else {
+                console.log(`${lib.fileName} deployment process exited successfully`);
                 resolve();
             }
         });
