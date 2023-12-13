6b1fce162b499bfed1386c74e31f74829d6036fd
Victor Mimo <victormimoc@gmail.com>
2023-12-13 16:25:19 -0500

    adding CapTable factory with Beacon Proxy

---
+++ chain/src/CapTableFactory.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { UpgradeableBeacon } from "openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import { BeaconProxy } from "openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import { Ownable } from "openzeppelin/contracts/access/Ownable.sol";
+
+contract CapTableFactory is Ownable {
+    address public capTableImplementation;
+    UpgradeableBeacon public capTableBeacon;
+    address[] public capTableProxies;
+
+    event CapTableCreated(address indexed capTableProxy);
+
+    constructor(address _capTableImplementation) Ownable() {
+        require(_capTableImplementation != address(0), "Invalid implementation address");
+        capTableImplementation = _capTableImplementation;
+        capTableBeacon = new UpgradeableBeacon(capTableImplementation);
+    }
+
+    function createCapTable(bytes memory initializationData) external onlyOwner returns (address) {
+        BeaconProxy capTableProxy = new BeaconProxy(address(capTableBeacon), initializationData);
+        capTableProxies.push(address(capTableProxy));
+        emit CapTableCreated(address(capTableProxy));
+        return address(capTableProxy);
+    }
+
+    function updateCapTableImplementation(address newImplementation) external onlyOwner {
+        // Add access control to restrict who can call this function
+        require(newImplementation != address(0), "Invalid implementation address");
+        capTableBeacon.upgradeTo(newImplementation);
+    }
+
+    function getCapTableCount() external view returns (uint256) {
+        return capTableProxies.length;
+    }
+}
