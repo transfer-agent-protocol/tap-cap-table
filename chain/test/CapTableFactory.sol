b310ed5015484ab11ec491a61b209c90a3c8b7a3
Victor Mimo <victormimoc@gmail.com>
2023-12-18 12:43:35 -0500

    testing updating

--- chain/test/CapTableFactory.sol
+++ chain/test/CapTableFactory.sol
@@ -39,24 +39,52 @@ contract CapTableFactoryTest is Test {
         assertEq(shares_issued, 0);
         assertEq(initial_shares_authorized, issuerInitialSharesAuthorized);
 
-        // // Verify by creating a stakeholder
-        // bytes16 stakeholderId = 0xd3373e0a4dd940000000000000000010;
-        // string memory stakeholderType = "INDIVIDUAL";
-        // string memory stakeholderRelationship = "INVESTOR";
+        // Verify by creating a stakeholder
+        bytes16 stakeholderId = 0xd3373e0a4dd940000000000000000010;
+        string memory stakeholderType = "INDIVIDUAL";
+        string memory stakeholderRelationship = "INVESTOR";
 
-        // capTable.createStakeholder(stakeholderId, stakeholderType, stakeholderRelationship);
+        capTable.createStakeholder(stakeholderId, stakeholderType, stakeholderRelationship);
 
-        // (bytes16 actualStakeolderId, string memory actualStakeholderType, string memory actualStakeholderRelationship) = capTable.getStakeholderById(
-        //     stakeholderId
-        // );
+        (bytes16 actualStakeolderId, string memory actualStakeholderType, string memory actualStakeholderRelationship) = capTable.getStakeholderById(
+            stakeholderId
+        );
 
-        // assertEq(actualStakeolderId, stakeholderId);
-        // assertEq(actualStakeholderType, stakeholderType);
-        // assertEq(actualStakeholderRelationship, stakeholderRelationship);
+        assertEq(actualStakeolderId, stakeholderId);
+        assertEq(actualStakeholderType, stakeholderType);
+        assertEq(actualStakeholderRelationship, stakeholderRelationship);
 
-        // // Testing that only owner can create stakeholder
-        // vm.prank(address(1));
-        // vm.expectRevert("Does not have admin role");
-        // capTable.createStakeholder(0xd3373e0a4dd940000000000000000005, "INDIVIDUAL", "EMPLOYEE");
+        // Testing that only owner can create stakeholder
+        vm.prank(address(1));
+        vm.expectRevert("Does not have admin role");
+        capTable.createStakeholder(0xd3373e0a4dd940000000000000000005, "INDIVIDUAL", "EMPLOYEE");
+    }
+
+    function testUpdateCapTableImplementation() public {
+        // Deploy new implementation
+        CapTable newCapTableImplementation = new CapTable();
+
+        // Update implementation
+        _capTableFactory.updateCapTableImplementation(address(newCapTableImplementation));
+
+        // Assert the implementation was updated
+        assertEq(address(_capTableFactory.capTableImplementation()), address(newCapTableImplementation));
+
+        // Create a cap table
+        bytes16 issuerId = 0xd3373e0a4dd9430f8a563281f2800e1e;
+        string memory issuerName = "Winston, Inc.";
+        uint256 issuerInitialSharesAuthorized = 1000000;
+
+        address capTableProxy = _capTableFactory.createCapTable(issuerId, issuerName, issuerInitialSharesAuthorized);
+
+        // Assert the cap table was created
+        CapTable capTable = CapTable(capTableProxy);
+
+        (bytes16 id, string memory name, uint256 shares_issued, uint256 initial_shares_authorized) = capTable.issuer();
+
+        assertEq(id, issuerId);
+        assertEq(name, issuerName);
+        assertEq(shares_issued, 0);
+        assertEq(initial_shares_authorized, issuerInitialSharesAuthorized);
     }
 }
