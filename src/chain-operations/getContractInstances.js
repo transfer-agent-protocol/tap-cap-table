6b45c065609175292e1a83fb274538dd535217f8
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2024-01-04 11:41:56 -0500

    Simplify getting contract instances and remove unused OP Goerli function

--- src/chain-operations/getContractInstances.js
+++ src/chain-operations/getContractInstances.js
@@ -5,46 +5,22 @@ import getTXLibContracts from "../utils/getLibrariesContracts.js";
 
 config();
 
-
-async function getLocalContractInstance(address) {
-    const CONTRACT_ADDRESS_LOCAL = address;
-
-    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_FAKE_ACCOUNT;
-    const LOCAL_RPC_URL = process.env.LOCAL_RPC_URL;
+async function getContractInstance(chain, address) {
+    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY;
+    const RPC_URL = process.env.RPC_URL;
+    const CHAIN_ID = parseInt(process.env.CHAIN_ID); // Convert to integer
 
     const customNetwork = {
-        chainId: 31337,
-        name: "local",
+        chainId: CHAIN_ID, // Use the CHAIN_ID from .env
+        name: chain, // Use the chain parameter as the network name
     };
 
-    const provider = new ethers.JsonRpcProvider(LOCAL_RPC_URL, customNetwork);
+    const provider = new ethers.JsonRpcProvider(RPC_URL, customNetwork);
     const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
-    const contract = new ethers.Contract(CONTRACT_ADDRESS_LOCAL, CAP_TABLE.abi, wallet);
-    const libraries = getTXLibContracts(contract.target, wallet)
+    const contract = new ethers.Contract(address, CAP_TABLE.abi, wallet);
+    const libraries = getTXLibContracts(contract.target, wallet);
+
     return { contract, provider, libraries };
-}
-
-async function getOptimismGoerliContractInstance(address) {
-    const CONTRACT_ADDRESS_OPTIMISM_GOERLI = address;
-    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_POET_TEST;
-
-    const provider = new ethers.JsonRpcProvider(process.env.L2_TESTNET_RPC_URL);
-    const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
-    const contract = new ethers.Contract(CONTRACT_ADDRESS_OPTIMISM_GOERLI, abi, wallet);
-    const libraries = getTXLibContracts(contract.target, wallet)
-
-
-    return { contract, provider, libraries};
-}
-
-async function getContractInstance(chain, address) {
-    if (chain === "local") {
-        return getLocalContractInstance(address);
-    } else if (chain === "optimism-goerli") {
-        return getOptimismGoerliContractInstance(address);
-    } else {
-        throw new Error(`Unsupported chain: ${chain}`);
-    }
 }
 
 export default getContractInstance;
