02bd7ea77583638483adfd12595e90ad6352950e
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2024-01-04 11:41:12 -0500

    Simplify and cleanup deployment

--- src/chain-operations/deployCapTable.js
+++ src/chain-operations/deployCapTable.js
@@ -6,36 +6,29 @@ import getTXLibContracts from "../utils/getLibrariesContracts.js";
 
 config();
 
-async function deployCapTableLocal(issuerId, issuerName, initial_shares_authorized) {
-    // Use environment variables for private key and provider endpoint
-    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_FAKE_ACCOUNT;
-    // Use the LOCAL_RPC_URL from .env
-    const LOCAL_RPC_URL = process.env.LOCAL_RPC_URL;
+async function deployCapTable(issuerId, issuerName, initial_shares_authorized) {
+    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY;
+    const RPC_URL = process.env.RPC_URL;
+    const CHAIN_ID = process.env.CHAIN_ID;
 
     const customNetwork = {
-        // TODO: handle changing Anvil's chain id better
-        // chainId: 31337,
-        // This one is Arbitrum Orbit's chain id
-        chainId: 32586980208,
-        name: "local",
+        // Change the CHAIN_ID in the .env file to deploy to a different network
+        chainId: parseInt(CHAIN_ID),
     };
 
-    // Use the LOCAL_RPC_URL for the provider
-    const provider = new ethers.JsonRpcProvider(LOCAL_RPC_URL, customNetwork);
+    const provider = new ethers.JsonRpcProvider(RPC_URL, customNetwork);
     const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
-    console.log("🗽 | Wallet address ", wallet.address);
+
+    console.log("🗽 | Wallet address: ", wallet.address);
 
     const factory = new ethers.ContractFactory(CAP_TABLE.abi, CAP_TABLE.bytecode, wallet);
-    console.log(
-        `✅ | Issuer id inside of deployment: ${issuerId},
-        ✅ | Issuer name inside of deployment: ${issuerName},
-        ✅ | With initial shares: ${initial_shares_authorized}`
-    );
-
     const contract = await factory.deploy(issuerId, issuerName, toScaledBigNumber(initial_shares_authorized));
 
     console.log("⏳ | Waiting for contract to be deployed...");
+
     const libraries = getTXLibContracts(contract.target, wallet);
+
+    console.log("✅ | Contract deployed!");
 
     return {
         contract,
@@ -45,30 +38,4 @@ async function deployCapTableLocal(issuerId, issuerName, initial_shares_authoriz
     };
 }
 
-async function deployCapTableOptimismGoerli(issuerId, issuerName, initial_shares_authorized) {
-    const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY_POET_TEST;
-
-    const provider = new ethers.JsonRpcProvider(process.env.L2_TESTNET_RPC_URL);
-    const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
-    const factory = new ethers.ContractFactory(CAP_TABLE.abi, CAP_TABLE.bytecode, wallet);
-    const contract = await factory.deploy(issuerId, issuerName, toScaledBigNumber(initial_shares_authorized));
-
-    console.log("⏳ | Waiting for contract to be deployed...");
-
-    console.log("✅ | Contract deployed!");
-
-    const libraries = getTXLibContracts(contract.target, wallet);
-
-    return { contract, provider, address: contract.target, libraries };
-}
-
-async function deployCapTable(chain, issuerId, issuerName, initial_shares_authorized) {
-    if (chain === "local") {
-        return deployCapTableLocal(issuerId, issuerName, initial_shares_authorized);
-    } else if (chain === "optimism-goerli") {
-        return deployCapTableOptimismGoerli(issuerId, issuerName, initial_shares_authorized);
-    } else {
-        throw new Error(`❌ | Unsupported chain: ${chain}`);
-    }
-}
 export default deployCapTable;
