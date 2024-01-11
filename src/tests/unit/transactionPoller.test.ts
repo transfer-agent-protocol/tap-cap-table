73b6063701e060f69fb3c909261eb9fa4c83747b
Kent Kolze <kent.kolze@gmail.com>
2024-01-11 12:45:38 -0600

    better docs and test

--- src/tests/unit/transactionPoller.test.ts
+++ src/tests/unit/transactionPoller.test.ts
@@ -2,7 +2,7 @@ import { trimEvents } from "../../chain-operations/transactionPoller";
 
 // TODO: if starts failing again: yarn add --dev jest-esm-transformer
 
-const myEvents = [5, 6, 6, 6, 7].map((x, i) => { return {blockNumber: x, i}; });
+const myEvents = [5, 6, 6, 6, 7, 7, 7].map((x, i) => { return {blockNumber: x, i}; });
 
 test('trimEvents partial', () => {
     const [events, block] = trimEvents(myEvents, 2, 10);
@@ -12,8 +12,9 @@ test('trimEvents partial', () => {
 });
 
 test('trimEvents full', () => {
-    for (const max of [5, 6, 15]) {
-        const [events, block] = trimEvents(myEvents, max, 10);
+    // We allow more than maxEvents in order to include all events of the last block
+    for (const maxEvents of [5, 6, 7, 15]) {
+        const [events, block] = trimEvents(myEvents, maxEvents, 10);
         expect(events.length).toBe(myEvents.length);
         expect(events).toStrictEqual(myEvents);
         expect(block).toBe(10);
