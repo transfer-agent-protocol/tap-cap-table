57e16e3e73f2e1a70721546207318c6922623103
Victor Mimo <victormimoc@gmail.com>
2024-01-19 16:38:04 -0300

    making finalized flag an argument

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
 import { startServer } from "./app.js";
 
-startServer();
+// Function to check if the flag is present
+const isFlagPresent = (flag) => process.argv.includes(flag);
+
+// Setting the default value of the flag to true
+const finalizedOnly = isFlagPresent("--finalized-only");
+
+console.log("Finalized Only:", finalizedOnly);
+
+startServer(finalizedOnly);
