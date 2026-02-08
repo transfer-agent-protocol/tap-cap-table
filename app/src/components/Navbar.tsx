fb81f5d7c4b5e97b721905978e44b3956a75030c
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2026-02-07 20:36:58 -0500

    hide mint nav link and add pre-launch warning

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,7 +1,21 @@
-import { Nav, Logotype } from "./wrappers";
+import styled from "styled-components";
+import { Nav } from "./wrappers";
 import { LogoRouter, StyledA } from "./buttons";
 import Link from "next/link";
 import Image from "next/image";
+
+const NavActions = styled.span`
+	display: flex;
+	flex-flow: row nowrap;
+	align-items: center;
+	gap: 0.25rem;
+`;
+
+const WalletWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	padding-left: 1rem;
+`;
 
 export default function Navbar() {
 	return (
@@ -11,17 +25,18 @@ export default function Navbar() {
 					<Image src="/tap-logo.svg" alt="Transfer Agent Protocol" width={48} height={48} />
 				</Link>
 			</LogoRouter>
-			<span>
+			<NavActions>
 				<StyledA>
-					<Link href="https://docs.transferagentprotocol.xyz" target="_blank">Docs
-					</Link>
+					<Link href="https://docs.transferagentprotocol.xyz" target="_blank">Docs</Link>
 				</StyledA>
 				<StyledA>
-					<Link href="https://github.com/transfer-agent-protocol/tap-cap-table" target="_blank ">
-						Github
-					</Link>
+					<Link href="https://github.com/transfer-agent-protocol/tap-cap-table" target="_blank">Github</Link>
 				</StyledA>
-			</span>
+				<WalletWrapper>
+				{/* @ts-expect-error — Reown web component */}
+					<appkit-button size="sm" />
+				</WalletWrapper>
+			</NavActions>
 		</Nav>
 	);
 };
