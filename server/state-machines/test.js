be21d2321915505bf8f8668c4e5935a921a3f5f9
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2026-02-14 10:52:25 -0500

    chore(server): remove dead and broken legacy scripts

--- server/state-machines/test.js
+++
@@ -1,6 +0,0 @@
-import processSM from "./process.js";
-import transactions from "../db/samples/data/Transactions.ocf.json" with { type: "json" };
-import manifest from "../db/samples/data/Manifest.ocf.json" with { type: "json" };
-import stockClasses from "../db/samples/data/StockClasses.ocf.json" with { type: "json" };
-
-processSM(manifest.issuer, transactions, stockClasses);
